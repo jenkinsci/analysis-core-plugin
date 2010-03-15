a32847aa9459ee227b6aa5c60ccb2058beee1b36
drulli <drulli@dev.java.net>
2010-03-15 18:48:26 +0000

    Added total number of warnings, links to plugin results and build result icons.

--- src/main/java/hudson/plugins/analysis/collector/dashboard/WarningsTablePortlet.java
+++ src/main/java/hudson/plugins/analysis/collector/dashboard/WarningsTablePortlet.java
@@ -7,6 +7,7 @@ import hudson.model.Job;
 import hudson.plugins.analysis.collector.AnalysisDescriptor;
 import hudson.plugins.analysis.collector.Messages;
 import hudson.plugins.analysis.core.AbstractProjectAction;
+import hudson.plugins.analysis.core.BuildResult;
 import hudson.plugins.checkstyle.CheckStyleProjectAction;
 import hudson.plugins.dry.DryProjectAction;
 import hudson.plugins.findbugs.FindBugsProjectAction;
@@ -15,6 +16,7 @@ import hudson.plugins.tasks.TasksProjectAction;
 import hudson.plugins.view.dashboard.DashboardPortlet;
 import hudson.plugins.warnings.WarningsProjectAction;
 
+import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
@@ -46,7 +48,7 @@ public class WarningsTablePortlet extends DashboardPortlet {
      */
     public String getCheckStyle(final Job<?, ?> job) {
         if (AnalysisDescriptor.isCheckStyleInstalled()) {
-            return getWarnings(job, CheckStyleProjectAction.class);
+            return getWarnings(job, CheckStyleProjectAction.class, "checkstyle");
         }
         return NO_RESULTS_FOUND;
     }
@@ -60,7 +62,7 @@ public class WarningsTablePortlet extends DashboardPortlet {
      */
     public String getDry(final Job<?, ?> job) {
         if (AnalysisDescriptor.isDryInstalled()) {
-            return getWarnings(job, DryProjectAction.class);
+            return getWarnings(job, DryProjectAction.class, "dry");
         }
         return NO_RESULTS_FOUND;
     }
@@ -74,7 +76,7 @@ public class WarningsTablePortlet extends DashboardPortlet {
      */
     public String getFindBugs(final Job<?, ?> job) {
         if (AnalysisDescriptor.isFindBugsInstalled()) {
-            return getWarnings(job, FindBugsProjectAction.class);
+            return getWarnings(job, FindBugsProjectAction.class, "findbugs");
         }
         return NO_RESULTS_FOUND;
     }
@@ -88,7 +90,7 @@ public class WarningsTablePortlet extends DashboardPortlet {
      */
     public String getPmd(final Job<?, ?> job) {
         if (AnalysisDescriptor.isPmdInstalled()) {
-            return getWarnings(job, PmdProjectAction.class);
+            return getWarnings(job, PmdProjectAction.class, "pmd");
         }
         return NO_RESULTS_FOUND;
     }
@@ -102,7 +104,21 @@ public class WarningsTablePortlet extends DashboardPortlet {
      */
     public String getTasks(final Job<?, ?> job) {
         if (AnalysisDescriptor.isOpenTasksInstalled()) {
-            return getWarnings(job, TasksProjectAction.class);
+            return getWarnings(job, TasksProjectAction.class, "tasks");
+        }
+        return NO_RESULTS_FOUND;
+    }
+
+    /**
+     * Returns the total number of warnings for the specified job.
+     *
+     * @param job
+     *            the job to get the warnings for
+     * @return the number of compiler warnings
+     */
+    public String getWarnings(final Job<?, ?> job) {
+        if (AnalysisDescriptor.isWarningsInstalled()) {
+            return getWarnings(job, WarningsProjectAction.class, "warnings");
         }
         return NO_RESULTS_FOUND;
     }
@@ -114,9 +130,53 @@ public class WarningsTablePortlet extends DashboardPortlet {
      *            the job to get the warnings for
      * @return the number of compiler warnings
      */
-    public String getWarnings(final Job<?, ?> job) {
-        if (AnalysisDescriptor.isWarningsInstalled()) {
-            return getWarnings(job, WarningsProjectAction.class);
+    public String getTotal(final Job<?, ?> job) {
+        return String.valueOf(
+                toInt(getCheckStyle(job))
+                + toInt(getDry(job))
+                + toInt(getFindBugs(job))
+                + toInt(getPmd(job))
+                + toInt(getTasks(job))
+                + toInt(getWarnings(job)));
+    }
+
+    /**
+     * Converts the string to an integer. If the string is not valid then 0
+     * is returned.
+     *
+     * @param value
+     *            the value to convert
+     * @return the integer value or 0
+     */
+    private int toInt(final String value) {
+        try {
+            return Integer.parseInt(StringUtils.substringBetween(value, ">", "<"));
+        }
+        catch (NumberFormatException exception) {
+            return 0;
+        }
+    }
+
+    /**
+     * Returns the warnings for the specified action.
+     *
+     * @param job
+     *            the job to get the action from
+     * @param actionType
+     *            the type of the action
+     * @param plugin
+     *            the plugin that is target of the link
+     * @return the number of warnings
+     */
+    private String getWarnings(final Job<?, ?> job, final Class<? extends AbstractProjectAction<?>> actionType, final String plugin) {
+        AbstractProjectAction<?> action = job.getAction(actionType);
+        if (action != null && action.hasValidResults()) {
+            BuildResult result = action.getLastAction().getResult();
+            String value = String.format("<a href=\"%s%s\">%d</a>", job.getShortUrl(), plugin, result.getNumberOfAnnotations());
+            if (result.isSuccessfulTouched()) {
+                return value + result.getResultIcon();
+            }
+            return value;
         }
         return NO_RESULTS_FOUND;
     }
@@ -130,12 +190,12 @@ public class WarningsTablePortlet extends DashboardPortlet {
      *            the type of the action
      * @return the number of warnings
      */
-    private String getWarnings(final Job<?, ?> job, final Class<? extends AbstractProjectAction<?>> actionType) {
+    private String getIcon(final Job<?, ?> job, final Class<? extends AbstractProjectAction<?>> actionType) {
         AbstractProjectAction<?> action = job.getAction(actionType);
         if (action != null && action.hasValidResults()) {
-            return Integer.toString(action.getLastAction().getResult().getNumberOfAnnotations());
+            return action.getLastAction().getResult().getResultIcon();
         }
-        return NO_RESULTS_FOUND;
+        return StringUtils.EMPTY;
     }
 
     /**
