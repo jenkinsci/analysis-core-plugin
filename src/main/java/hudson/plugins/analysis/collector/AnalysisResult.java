4975038ec75b7f6a0ddf515c46d278e19d95e9b6
drulli <drulli@dev.java.net>
2010-05-20 18:20:52 +0000

    Replaced broken newInstance method with @Extension(optional = true).

--- src/main/java/hudson/plugins/analysis/collector/AnalysisResult.java
+++ src/main/java/hudson/plugins/analysis/collector/AnalysisResult.java
@@ -22,6 +22,7 @@ public class AnalysisResult extends BuildResult {
     private static final long serialVersionUID = 847650789493429154L;
 
     /** Number of annotations by origin mapping. */
+    // FIXME: check if we need a readResolve method?
     private final Map<String, Integer> annotationsByOrigin = Maps.newHashMap();
 
     /**
