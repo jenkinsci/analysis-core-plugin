5f92bbdec7def5782abcc4b51e96675600750c35
drulli <drulli@dev.java.net>
2011-01-27 13:01:36 +0000

    Added collected modules to aggregated result.

--- src/main/java/hudson/plugins/analysis/collector/AnalysisAnnotationsAggregator.java
+++ src/main/java/hudson/plugins/analysis/collector/AnalysisAnnotationsAggregator.java
@@ -8,9 +8,6 @@ import hudson.model.BuildListener;
 import hudson.plugins.analysis.core.AnnotationsAggregator;
 import hudson.plugins.analysis.core.HealthDescriptor;
 import hudson.plugins.analysis.core.ParserResult;
-import hudson.plugins.analysis.util.model.FileAnnotation;
-
-import java.util.Collection;
 
 /**
  * Aggregates {@link AnalysisResultAction}s of {@link MatrixRun}s into
@@ -48,10 +45,10 @@ public class AnalysisAnnotationsAggregator extends AnnotationsAggregator {
 
     /** {@inheritDoc} */
     @Override
-    protected Collection<? extends FileAnnotation> getAnnotations(final MatrixRun run) {
+    protected AnalysisResult getResult(final MatrixRun run) {
         AnalysisResultAction action = run.getAction(AnalysisResultAction.class);
 
-        return action.getResult().getAnnotations();
+        return action.getResult();
     }
 }
 
