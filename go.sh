1b825cf691e6cd06018eb43e883e04606a9971a4
J.Gogic <gogic@hm.edu>
2017-02-15 20:06:13 +0100

    Delete go.sh

--- go.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-rm -rf $JENKINS_HOME/plugins/analysis-core*
-
-mvn install || { echo "Build failed"; exit 1; }
-
-cp -f target/analysis-core.hpi $JENKINS_HOME/plugins/
-
-cd $JENKINS_HOME
-./go.sh
