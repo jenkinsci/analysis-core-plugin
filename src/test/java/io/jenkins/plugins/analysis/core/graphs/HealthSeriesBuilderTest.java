6c642c7315070c31d1e98381070c8ff4fd9dc376
Tom <weissbier.weissbier@googlemail.com>
2017-11-09 11:29:55 +0100

    added test
    - HealthSeriesBuilderTest

---
+++ src/test/java/io/jenkins/plugins/analysis/core/graphs/HealthSeriesBuilderTest.java
@@ -0,0 +1,41 @@
+package io.jenkins.plugins.analysis.core.graphs;
+
+import java.util.List;
+
+import org.jfree.data.category.CategoryDataset;
+import org.junit.jupiter.api.Test;
+
+import com.google.common.collect.Lists;
+
+import io.jenkins.plugins.analysis.core.quality.HealthDescriptor;
+import io.jenkins.plugins.analysis.core.quality.StaticAnalysisRun;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class HealthSeriesBuilderTest {
+
+
+    /** Verifies that the health descriptor is not enabled */
+    @Test
+    void healthDescriptorIsNotEnabled() {
+        HealthDescriptor healthDescriptor = mock(HealthDescriptor.class);
+        when(healthDescriptor.isEnabled()).thenReturn(false);
+
+        StaticAnalysisRun buildResult = mock(StaticAnalysisRun.class);
+        when(buildResult.getTotalSize()).thenReturn(3);
+
+        HealthSeriesBuilder healthSeriesBuilder = new HealthSeriesBuilder(healthDescriptor);
+        List<Integer> list = healthSeriesBuilder.computeSeries(buildResult);
+
+        assertThat(list.get(0)).isEqualTo(3);
+    }
+
+    private GraphConfiguration createConfiguration() {
+        return mock(GraphConfiguration.class);
+    }
+
+
+
+
+}
