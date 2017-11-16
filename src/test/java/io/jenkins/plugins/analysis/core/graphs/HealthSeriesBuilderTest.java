8cb2c61172bc5a6b56703e3692e15797a044dd8f
Johdeath <johannes.arzt@gmx.net>
2017-11-16 09:31:34 +0100

    add first mutlti series test for the HealthSeriesBuilder

--- src/test/java/io/jenkins/plugins/analysis/core/graphs/HealthSeriesBuilderTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/graphs/HealthSeriesBuilderTest.java
@@ -1,15 +1,21 @@
 package io.jenkins.plugins.analysis.core.graphs;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import org.jfree.data.category.CategoryDataset;
 import org.junit.jupiter.api.Test;
+
+import io.jenkins.plugins.analysis.core.quality.AnalysisBuild;
 import io.jenkins.plugins.analysis.core.quality.HealthDescriptor;
 import io.jenkins.plugins.analysis.core.quality.StaticAnalysisRun;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-/** Unit tests for the {@link HealthSeriesBuilder}.
+/**
+ * Unit tests for the {@link HealthSeriesBuilder}.
  *
- *@author Johannes Arzt
- *
+ * @author Johannes Arzt
  */
 
 class HealthSeriesBuilderTest {
@@ -94,6 +100,39 @@ class HealthSeriesBuilderTest {
 
     }
 
+    @Test
+    void daySeriesTest() {
+        HealthDescriptor health = mock(HealthDescriptor.class);
+        when(health.isEnabled()).thenReturn(true);
+        when(health.getHealthy()).thenReturn(10);
+        when(health.getUnHealthy()).thenReturn(20);
+
+
+        HealthSeriesBuilder builder = new HealthSeriesBuilder(health);
+
+        GraphConfiguration graphConfig = mock(GraphConfiguration.class);
+        when(graphConfig.useBuildDateAsDomain()).thenReturn(false);
+
+        List<StaticAnalysisRun> runs = new ArrayList<>();
+
+        for (int i = 1; i <= 4; i++) {
+            StaticAnalysisRun buildResult = mock(StaticAnalysisRun.class);
+            when(buildResult.getTotalSize()).thenReturn(i * 10);
+            AnalysisBuild build = mock(AnalysisBuild.class);
+            when(build.getNumber()).thenReturn(i);
+            when(buildResult.getBuild()).thenReturn(build);
+            runs.add(buildResult);
+
+        }
+
+
+        CategoryDataset dataset = builder.createDataSet(graphConfig, runs);
+
+       System.out.print("for debug");
+
+
+
+    }
 
 
 }
