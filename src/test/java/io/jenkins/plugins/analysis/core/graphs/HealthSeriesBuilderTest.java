48d12fb8bd26722b1889b3db89fc7b3e7ad1d631
Unknown <balz@hm.edu>
2017-11-19 14:10:21 +0100

    add test cases

---
+++ src/test/java/io/jenkins/plugins/analysis/core/graphs/HealthSeriesBuilderTest.java
@@ -0,0 +1,168 @@
+package io.jenkins.plugins.analysis.core.graphs;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import io.jenkins.plugins.analysis.core.quality.HealthDescriptor;
+import io.jenkins.plugins.analysis.core.quality.StaticAnalysisRun;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+/**
+ * Test Class for HealthSeriesBuilder.
+ */
+class HealthSeriesBuilderTest {
+
+
+    /**
+     * generate a stub parameter.
+     * @return stub parameter
+     */
+    private StaticAnalysisRun generateParamStub() {
+        StaticAnalysisRun paramStub = mock(StaticAnalysisRun.class);
+        when(paramStub.getTotalSize()).thenReturn(0).thenReturn(1).thenReturn(10).thenReturn(20);
+        return paramStub;
+    }
+    @Test
+    void healthReportWithDisabledHealthDescriptor() {
+        HealthDescriptor healthDiscStub = GenerateHealthStub.generateHealthDescriptor(false, 0, 0);
+        StaticAnalysisRun paramStub = generateParamStub();
+        HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
+
+        assertThat(b.computeSeries(paramStub)).containsExactly(0);
+        assertThat(b.computeSeries(paramStub)).containsExactly(1);
+        assertThat(b.computeSeries(paramStub)).containsExactly(10);
+        assertThat(b.computeSeries(paramStub)).containsExactly(20);
+    }
+    @Test
+    void healthReportWithDisabledHealthDescriptorAndHealthyDefinition() {
+        HealthDescriptor healthDiscStub = GenerateHealthStub.generateHealthDescriptor(false, 9, 10);
+        StaticAnalysisRun paramStub = generateParamStub();
+        HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
+
+        List<Integer> firstList = b.computeSeries(paramStub);
+        List<Integer> secondList = b.computeSeries(paramStub);
+        List<Integer> thirdList = b.computeSeries(paramStub);
+        List<Integer> fourthList = b.computeSeries(paramStub);
+
+        assertThat(firstList).containsExactly(0);
+        assertThat(secondList).containsExactly(1);
+        assertThat(thirdList).containsExactly(10);
+        assertThat(fourthList).containsExactly(20);
+
+    }
+    @Test
+    void healthReportWithNoPreviousHeath() {
+        HealthDescriptor healthDiscStub = GenerateHealthStub.generateHealthDescriptor(true, 0, 0);
+        StaticAnalysisRun paramStub = generateParamStub();
+        HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
+
+        List<Integer> firstList = b.computeSeries(paramStub);
+        List<Integer> secondList = b.computeSeries(paramStub);
+        List<Integer> thirdList = b.computeSeries(paramStub);
+        List<Integer> fourthList = b.computeSeries(paramStub);
+
+        assertThat(firstList).containsExactly(0, 0, 0);
+        assertThat(secondList).containsExactly(0, 0, 1);
+        assertThat(thirdList).containsExactly(0, 0, 10);
+        assertThat(fourthList).containsExactly(0, 0, 20);
+    }
+
+    @Test
+    void healthReportHealthyEqUnhealthy() {
+        HealthDescriptor healthDiscStub = GenerateHealthStub.generateHealthDescriptor(true, 9, 9);
+        StaticAnalysisRun paramStub = generateParamStub();
+        HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
+
+        List<Integer> firstList = b.computeSeries(paramStub);
+        List<Integer> secondList = b.computeSeries(paramStub);
+        List<Integer> thirdList = b.computeSeries(paramStub);
+        List<Integer> fourthList = b.computeSeries(paramStub);
+
+        assertThat(firstList).containsExactly(0, 0, 0);
+        assertThat(secondList).containsExactly(1, 0, 0);
+        assertThat(thirdList).containsExactly(9, 0, 1);
+        assertThat(fourthList).containsExactly(9, 0, 11);
+
+    }
+    @Test
+    void healthReportHealthyOneLessUnhealthy() {
+        HealthDescriptor healthDiscStub = GenerateHealthStub.generateHealthDescriptor(true, 9, 10);
+        StaticAnalysisRun paramStub = generateParamStub();
+        HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
+
+        List<Integer> firstList = b.computeSeries(paramStub);
+        List<Integer> secondList = b.computeSeries(paramStub);
+        List<Integer> thirdList = b.computeSeries(paramStub);
+        List<Integer> fourthList = b.computeSeries(paramStub);
+
+        assertThat(firstList).containsExactly(0, 0, 0);
+        assertThat(secondList).containsExactly(1, 0, 0);
+        assertThat(thirdList).containsExactly(9, 1, 0);
+        assertThat(fourthList).containsExactly(9, 1, 10);
+
+    }
+    @Test
+    void healthReportMoreUnhealthyThanHealthy() {
+        HealthDescriptor healthDiscStub = GenerateHealthStub.generateHealthDescriptor(true, 9, 15);
+        StaticAnalysisRun paramStub = generateParamStub();
+        HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
+
+        List<Integer> firstList = b.computeSeries(paramStub);
+        List<Integer> secondList = b.computeSeries(paramStub);
+        List<Integer> thirdList = b.computeSeries(paramStub);
+        List<Integer> fourthList = b.computeSeries(paramStub);
+
+        assertThat(firstList).containsExactly(0, 0, 0);
+        assertThat(secondList).containsExactly(1, 0, 0);
+        assertThat(thirdList).containsExactly(9, 1, 0);
+        assertThat(fourthList).containsExactly(9, 6, 5);
+
+    }
+
+
+
+
+    /*
+     * Should never be reached because unhealthy < healthy
+     */
+    @Test
+    void healthReportWithWithJustHealthy() {
+        HealthDescriptor healthDiscStub = GenerateHealthStub.generateHealthDescriptor(true, 10, 0);
+        StaticAnalysisRun paramStub = generateParamStub();
+        HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
+
+        List<Integer> firstList = b.computeSeries(paramStub);
+        List<Integer> secondList = b.computeSeries(paramStub);
+        List<Integer> thirdList = b.computeSeries(paramStub);
+        List<Integer> fourthList = b.computeSeries(paramStub);
+
+        assertThat(firstList).containsExactly(0, 0, 0);
+        assertThat(secondList).containsExactly(1, 0, 0);
+        assertThat(thirdList).containsExactly(10, 0, 0);
+        assertThat(fourthList).containsExactly(10, 0, 0);
+
+    }
+    /*
+         * Should also never be reached because unhealthy and healthy < 0
+         */
+    @Test
+    void healthReportWithNegativeNumbers() {
+        HealthDescriptor healthDiscStub = GenerateHealthStub.generateHealthDescriptor(true, -4, -3);
+        StaticAnalysisRun paramStub = mock(StaticAnalysisRun.class);
+        when(paramStub.getTotalSize()).thenReturn(-0).thenReturn(-1).thenReturn(-5).thenReturn(-10);
+        HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
+
+        List<Integer> firstList = b.computeSeries(paramStub);
+        List<Integer> secondList = b.computeSeries(paramStub);
+        List<Integer> thirdList = b.computeSeries(paramStub);
+        List<Integer> fourthList = b.computeSeries(paramStub);
+
+        assertThat(firstList).containsExactly(-4, 1, 3);
+        assertThat(secondList).containsExactly(-4, 1, 2);
+        assertThat(thirdList).containsExactly(-5, 0, 0);
+        assertThat(fourthList).containsExactly(-10, 0, 0);
+
+    }
+}
