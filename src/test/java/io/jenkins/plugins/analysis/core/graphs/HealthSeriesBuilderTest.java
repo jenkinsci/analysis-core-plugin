980733a2ee264b97ba4dc7118face7e6be356ac7
Marcel Binder <binder4@hm.edu>
2017-11-20 22:37:00 +0100

    Added parametrized tests for computeSeries and computeDataSet.

--- src/test/java/io/jenkins/plugins/analysis/core/graphs/HealthSeriesBuilderTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/graphs/HealthSeriesBuilderTest.java
@@ -1,17 +1,21 @@
 package io.jenkins.plugins.analysis.core.graphs;
 
+import java.time.LocalDate;
+import java.time.ZoneOffset;
 import java.util.List;
 import java.util.stream.Stream;
 
+import org.jfree.data.category.CategoryDataset;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import static io.jenkins.plugins.analysis.core.graphs.assertj.Assertions.*;
+import io.jenkins.plugins.analysis.core.quality.AnalysisBuild;
 import io.jenkins.plugins.analysis.core.quality.HealthDescriptor;
 import io.jenkins.plugins.analysis.core.quality.StaticAnalysisRun;
 import static java.util.Arrays.*;
 import static java.util.Collections.*;
-import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 /**
@@ -20,17 +24,29 @@ import static org.mockito.Mockito.*;
  * @author Marcel Binder
  */
 class HealthSeriesBuilderTest {
+    private static final LocalDate TODAY = LocalDate.now();
+    private static final LocalDate YESTERDAY = TODAY.minusDays(1);
+    private static final LocalDate TWO_DAYS_AGO = TODAY.minusDays(2);
+    private static final ResultTime RESULT_TIME_TODAY = new ResultTime(TODAY);
     private static final int HEALTHY_THRESHOLD = 2;
     private static final int UNHEALTHY_THRESHOLD = 8;
+    private static final int BELOW_HEALTHY_THRESHOLD = HEALTHY_THRESHOLD - 1;
+    private static final int ABOVE_HEALTHY_THRESHOLD = HEALTHY_THRESHOLD + 1;
+    private static final int BELOW_UNHEALTHY_THRESHOLD = UNHEALTHY_THRESHOLD - 1;
+    private static final int ABOVE_UNHEALTHY_THRESHOLD = UNHEALTHY_THRESHOLD + 1;
+    private static final List<Integer> SERIES_BELOW_HEALTHY_THRESHOLD = asList(BELOW_HEALTHY_THRESHOLD, 0, 0);
+    private static final List<Integer> SERIES_AT_HEALTHY_THRESHOLD = asList(HEALTHY_THRESHOLD, 0, 0);
+    private static final List<Integer> SERIES_ABOVE_HEALTHY_THRESHOLD = asList(HEALTHY_THRESHOLD, ABOVE_HEALTHY_THRESHOLD - HEALTHY_THRESHOLD, 0);
+    private static final List<Integer> SERIES_BELOW_UNHEALTHY_THRESHOLD = asList(HEALTHY_THRESHOLD, BELOW_UNHEALTHY_THRESHOLD - HEALTHY_THRESHOLD, 0);
+    private static final List<Integer> SERIES_AT_UNHEALTHY_THRESHOLD = asList(HEALTHY_THRESHOLD, UNHEALTHY_THRESHOLD - HEALTHY_THRESHOLD, 0);
+    private static final List<Integer> SERIES_ABOVE_UNHEALTHY_THRESHOLD = asList(HEALTHY_THRESHOLD, UNHEALTHY_THRESHOLD - HEALTHY_THRESHOLD, ABOVE_UNHEALTHY_THRESHOLD - UNHEALTHY_THRESHOLD);
 
     @ParameterizedTest(name = "{0}")
     @MethodSource("computeSeriesArguments")
-    void computeSeries(final String testDescription, final boolean reportingEnabled, final int issueCount, final List<Integer> expectedSeries) {
-        HealthDescriptor healthDescriptor = createHealthDescriptor(reportingEnabled);
+    void computeSeries(final String testDescription, final HealthDescriptor healthDescriptor, final StaticAnalysisRun result, final List<Integer> expectedSeries) {
         HealthSeriesBuilder builder = createBuilder(healthDescriptor);
-        StaticAnalysisRun run = createResult(issueCount);
 
-        List<Integer> series = builder.computeSeries(run);
+        List<Integer> series = builder.computeSeries(result);
 
         assertThat(series).isEqualTo(expectedSeries);
     }
@@ -39,46 +55,238 @@ class HealthSeriesBuilderTest {
         return Stream.of(
                 Arguments.of(
                         "reporting disabled",
-                        false, 5, singletonList(5)),
+                        reportingDisabled(),
+                        result(ABOVE_UNHEALTHY_THRESHOLD),
+                        singletonList(ABOVE_UNHEALTHY_THRESHOLD)),
                 Arguments.of(
-                        "issueCount == 0",
-                        true, 0, asList(0, 0, 0)),
+                        "issueCount = 0",
+                        reportingEnabled(),
+                        result(0),
+                        asList(0, 0, 0)),
                 Arguments.of(
                         "issueCount < HEALTHY_THRESHOLD",
-                        true, HEALTHY_THRESHOLD - 1, asList(HEALTHY_THRESHOLD - 1, 0, 0)),
+                        reportingEnabled(),
+                        result(BELOW_HEALTHY_THRESHOLD),
+                        SERIES_BELOW_HEALTHY_THRESHOLD),
                 Arguments.of(
-                        "issueCount == HEALTHY_THRESHOLD",
-                        true, HEALTHY_THRESHOLD, asList(HEALTHY_THRESHOLD, 0, 0)),
+                        "issueCount = HEALTHY_THRESHOLD",
+                        reportingEnabled(),
+                        result(HEALTHY_THRESHOLD),
+                        SERIES_AT_HEALTHY_THRESHOLD),
                 Arguments.of(
                         "issueCount > HEALTHY_THRESHOLD",
-                        true, HEALTHY_THRESHOLD + 1, asList(HEALTHY_THRESHOLD, 1, 0)),
+                        reportingEnabled(),
+                        result(ABOVE_HEALTHY_THRESHOLD),
+                        SERIES_ABOVE_HEALTHY_THRESHOLD),
                 Arguments.of(
                         "issueCount < UNHEALTHY_THRESHOLD",
-                        true, UNHEALTHY_THRESHOLD - 1, asList(HEALTHY_THRESHOLD, UNHEALTHY_THRESHOLD - HEALTHY_THRESHOLD - 1, 0)),
+                        reportingEnabled(),
+                        result(BELOW_UNHEALTHY_THRESHOLD),
+                        SERIES_BELOW_UNHEALTHY_THRESHOLD),
                 Arguments.of(
-                        "issueCount == UNHEALTHY_THRESHOLD",
-                        true, UNHEALTHY_THRESHOLD, asList(HEALTHY_THRESHOLD, UNHEALTHY_THRESHOLD - HEALTHY_THRESHOLD, 0)),
+                        "issueCount = UNHEALTHY_THRESHOLD",
+                        reportingEnabled(),
+                        result(UNHEALTHY_THRESHOLD),
+                        SERIES_AT_UNHEALTHY_THRESHOLD),
                 Arguments.of(
                         "issueCount > UNHEALTHY_COUNT",
-                        true, UNHEALTHY_THRESHOLD + 1, asList(HEALTHY_THRESHOLD, UNHEALTHY_THRESHOLD - HEALTHY_THRESHOLD, 1))
+                        reportingEnabled(),
+                        result(ABOVE_UNHEALTHY_THRESHOLD),
+                        SERIES_ABOVE_UNHEALTHY_THRESHOLD)
         );
     }
 
-    private HealthDescriptor createHealthDescriptor(final boolean reportingEnabled) {
+    private static HealthDescriptor reportingDisabled() {
+        return mock(HealthDescriptor.class);
+    }
+
+    private static HealthDescriptor reportingEnabled() {
         HealthDescriptor healthDescriptor = mock(HealthDescriptor.class);
-        when(healthDescriptor.isEnabled()).thenReturn(reportingEnabled);
+        when(healthDescriptor.isEnabled()).thenReturn(true);
         when(healthDescriptor.getHealthy()).thenReturn(HEALTHY_THRESHOLD);
         when(healthDescriptor.getUnHealthy()).thenReturn(UNHEALTHY_THRESHOLD);
         return healthDescriptor;
     }
 
     private HealthSeriesBuilder createBuilder(HealthDescriptor healthDescriptor) {
-        return new HealthSeriesBuilder(healthDescriptor);
+        return new HealthSeriesBuilder(healthDescriptor, RESULT_TIME_TODAY);
     }
 
-    private StaticAnalysisRun createResult(final int totalSize) {
+    private static StaticAnalysisRun result(final int totalSize) {
         StaticAnalysisRun run = mock(StaticAnalysisRun.class);
         when(run.getTotalSize()).thenReturn(totalSize);
         return run;
+    }
+
+    private static StaticAnalysisRun result(final AnalysisBuild build, final int totalSize) {
+        StaticAnalysisRun result = result(totalSize);
+        when(result.getBuild()).thenReturn(build);
+        return result;
+    }
+
+    private static AnalysisBuild build(final int buildNumber) {
+        AnalysisBuild build = mock(AnalysisBuild.class);
+        when(build.getNumber()).thenReturn(buildNumber);
+        return build;
+    }
+
+    private static AnalysisBuild build(final int buildNumber, final LocalDate buildTime) {
+        AnalysisBuild build = build(buildNumber);
+        when(build.getTimeInMillis()).thenReturn(buildTime.atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli());
+        return build;
+    }
+
+    private static AnalysisBuild build(final LocalDate buildTime) {
+        AnalysisBuild build = mock(AnalysisBuild.class);
+        when(build.getTimeInMillis()).thenReturn(buildTime.atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli());
+        return build;
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("createDataSetArguments")
+    void createDataSet(final String testDescription, final GraphConfiguration configuration, List<StaticAnalysisRun> results, List<List<Integer>> expectedSeries) {
+        SeriesBuilder builder = createBuilder(reportingEnabled());
+
+        CategoryDataset dataSet = builder.createDataSet(configuration, results);
+
+        assertThat(dataSet).hasValues(expectedSeries);
+    }
+
+    private static Stream<Object> createDataSetArguments() {
+        return Stream.of(
+                Arguments.of(
+                        "use build number, no results",
+                        buildNumberDomain(),
+                        emptyList(),
+                        emptyList()),
+                Arguments.of(
+                        "use build number, single results",
+                        buildNumberDomain(),
+                        singletonList(result(build(1), HEALTHY_THRESHOLD)),
+                        singletonList(SERIES_AT_HEALTHY_THRESHOLD)),
+                Arguments.of(
+                        "use build number, single results, day count, build not too old",
+                        dayCountSingleDay(),
+                        singletonList(result(build(1, YESTERDAY), HEALTHY_THRESHOLD)),
+                        singletonList(SERIES_AT_HEALTHY_THRESHOLD)),
+                Arguments.of(
+                        "use build number, single results, day count, build too old",
+                        dayCountSingleDay(),
+                        singletonList(
+                                result(build(1, TWO_DAYS_AGO), HEALTHY_THRESHOLD)
+                        ),
+                        emptyList()),
+                Arguments.of(
+                        "use build number, single results, build count = 0, not filtered",
+                        buildCount(0),
+                        singletonList(result(build(1), HEALTHY_THRESHOLD)),
+                        singletonList(SERIES_AT_HEALTHY_THRESHOLD)),
+                Arguments.of(
+                        "use build number, single results, build count = 1, not filtered",
+                        buildCount(1),
+                        singletonList(result(build(1), HEALTHY_THRESHOLD)),
+                        singletonList(SERIES_AT_HEALTHY_THRESHOLD)),
+                Arguments.of(
+                        "use build number, two results",
+                        buildNumberDomain(),
+                        asList(
+                                result(build(2), BELOW_HEALTHY_THRESHOLD),
+                                result(build(1), HEALTHY_THRESHOLD)
+                        ),
+                        asList(
+                                SERIES_AT_HEALTHY_THRESHOLD,
+                                SERIES_BELOW_HEALTHY_THRESHOLD
+                        )),
+                Arguments.of(
+                        "use build number, two results on different days, day count = 1",
+                        dayCountSingleDay(),
+                        asList(
+                                result(build(2, YESTERDAY), BELOW_HEALTHY_THRESHOLD),
+                                result(build(1, TWO_DAYS_AGO), HEALTHY_THRESHOLD)
+                        ),
+                        singletonList(SERIES_BELOW_HEALTHY_THRESHOLD)),
+                Arguments.of(
+                        "use build number, three results, build count = 2",
+                        buildCount(2),
+                        asList(
+                                result(build(3), BELOW_HEALTHY_THRESHOLD),
+                                result(build(2), HEALTHY_THRESHOLD),
+                                result(build(1), ABOVE_HEALTHY_THRESHOLD)
+                        ),
+                        asList(
+                                SERIES_AT_HEALTHY_THRESHOLD,
+                                SERIES_BELOW_HEALTHY_THRESHOLD
+                        )),
+                Arguments.of(
+                        "use build date, single day, single result",
+                        buildDateDomain(),
+                        singletonList(
+                                result(build(TODAY), HEALTHY_THRESHOLD)
+                        ),
+                        singletonList(SERIES_AT_HEALTHY_THRESHOLD)),
+                Arguments.of(
+                        "use build date, single day, multiple results",
+                        buildDateDomain(),
+                        asList(
+                                result(build(TODAY), BELOW_HEALTHY_THRESHOLD),
+                                result(build(TODAY), HEALTHY_THRESHOLD)
+                        ),
+                        singletonList(
+                                asList(3 / 2, 0, 0)
+                        )),
+                Arguments.of(
+                        "use build date, multiple days, single result per day",
+                        buildDateDomain(),
+                        asList(
+                                result(build(TODAY), BELOW_HEALTHY_THRESHOLD),
+                                result(build(YESTERDAY), HEALTHY_THRESHOLD),
+                                result(build(TWO_DAYS_AGO), ABOVE_HEALTHY_THRESHOLD)
+                        ),
+                        asList(
+                                SERIES_ABOVE_HEALTHY_THRESHOLD,
+                                SERIES_AT_HEALTHY_THRESHOLD,
+                                SERIES_BELOW_HEALTHY_THRESHOLD
+                        )),
+                Arguments.of(
+                        "use build date, multiple days, multiple results per day",
+                        buildDateDomain(),
+                        asList(
+                                result(build(TODAY), 1),
+                                result(build(TODAY), 2),
+                                result(build(YESTERDAY), 4),
+                                result(build(TWO_DAYS_AGO), 5),
+                                result(build(TWO_DAYS_AGO), 6),
+                                result(build(TWO_DAYS_AGO), 9)
+                        ),
+                        asList(
+                                asList(2, 13 / 3, 1 / 3),
+                                asList(2, 2, 0),
+                                asList(3 / 2, 0, 0)
+                        ))
+        );
+    }
+
+    private static GraphConfiguration buildNumberDomain() {
+        return mock(GraphConfiguration.class);
+    }
+
+    private static GraphConfiguration buildDateDomain() {
+        GraphConfiguration configuration = mock(GraphConfiguration.class);
+        when(configuration.useBuildDateAsDomain()).thenReturn(true);
+        return configuration;
+    }
+
+    private static GraphConfiguration buildCount(final int buildCount) {
+        GraphConfiguration configuration = buildNumberDomain();
+        when(configuration.isBuildCountDefined()).thenReturn(true);
+        when(configuration.getBuildCount()).thenReturn(buildCount);
+        return configuration;
+    }
+
+    private static GraphConfiguration dayCountSingleDay() {
+        GraphConfiguration configuration = buildNumberDomain();
+        when(configuration.isDayCountDefined()).thenReturn(true);
+        when(configuration.getDayCount()).thenReturn(1);
+        return configuration;
     }
 }
