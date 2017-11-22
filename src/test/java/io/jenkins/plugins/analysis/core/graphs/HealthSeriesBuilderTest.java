bb37e3a4b265dbce4ed0fa4e3e5861b5c4257f2e
aykut <aykut.yilmaz0@hm.edu>
2017-11-22 15:35:16 +0100

    + HealthSeriesBuilderTest: more SeriesBuilder tests

--- src/test/java/io/jenkins/plugins/analysis/core/graphs/HealthSeriesBuilderTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/graphs/HealthSeriesBuilderTest.java
@@ -1,19 +1,18 @@
 package io.jenkins.plugins.analysis.core.graphs;
 
-import com.google.common.collect.Lists;
 import hudson.util.DataSetBuilder;
+import io.jenkins.plugins.analysis.core.history.ResultHistory;
 import io.jenkins.plugins.analysis.core.quality.AnalysisBuild;
 import io.jenkins.plugins.analysis.core.quality.HealthDescriptor;
 import io.jenkins.plugins.analysis.core.quality.StaticAnalysisRun;
+import io.jenkins.plugins.analysis.core.steps.AnalysisResult;
 import org.jfree.data.category.CategoryDataset;
-import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
-
-import org.joda.time.LocalDate;
 
 import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -40,26 +39,22 @@ class HealthSeriesBuilderTest {
     private static final GraphConfiguration CONFIGURATION = mock(GraphConfiguration.class);
     private static final Iterable<? extends StaticAnalysisRun> EMPTY_RESULTS = Collections.emptyList();
 
-    private static final SeriesBuilder SERIES_BUILDER = new HealthSeriesBuilder(HEALTH_DESCRIPTOR);
-
     private static final Iterable<? extends StaticAnalysisRun> RESULTS = Arrays.asList(
             STATIC_ANALYSIS_RUN,
             STATIC_ANALYSIS_RUN_2,
             STATIC_ANALYSIS_RUN_3
     );
 
-
     @Test
     void disabledHealthDescriptorWhereBuilderReturnsZeroOneOrTwo() {
-        StaticAnalysisRun staticAnalysisRun = mock(StaticAnalysisRun.class);
 
         when(HEALTH_DESCRIPTOR.isEnabled()).thenReturn(false);
-        when(staticAnalysisRun.getTotalSize()).thenReturn(0, 1, 2);
+        when(STATIC_ANALYSIS_RUN.getTotalSize()).thenReturn(0, 1, 2);
         HealthSeriesBuilder builder = new HealthSeriesBuilder(HEALTH_DESCRIPTOR);
 
-        List<Integer> justZero = builder.computeSeries(staticAnalysisRun);
-        List<Integer> justOne = builder.computeSeries(staticAnalysisRun);
-        List<Integer> justTwo = builder.computeSeries(staticAnalysisRun);
+        List<Integer> justZero = builder.computeSeries(STATIC_ANALYSIS_RUN);
+        List<Integer> justOne = builder.computeSeries(STATIC_ANALYSIS_RUN);
+        List<Integer> justTwo = builder.computeSeries(STATIC_ANALYSIS_RUN);
 
         assertThat(justZero.get(0)).isEqualTo(0);
         assertThat(justOne.get(0)).isEqualTo(1);
@@ -68,20 +63,18 @@ class HealthSeriesBuilderTest {
 
     @Test
     void remainderBiggerZero() {
-        StaticAnalysisRun staticAnalysisRun = mock(StaticAnalysisRun.class);
-
         when(HEALTH_DESCRIPTOR.isEnabled()).thenReturn(true);
 
-        when(staticAnalysisRun.getTotalSize()).thenReturn(6);
+        when(STATIC_ANALYSIS_RUN.getTotalSize()).thenReturn(6);
         when(HEALTH_DESCRIPTOR.getUnHealthy()).thenReturn(10);
         when(HEALTH_DESCRIPTOR.getHealthy()).thenReturn(5);
 
         HealthSeriesBuilder builder = new HealthSeriesBuilder(HEALTH_DESCRIPTOR);
-        List<Integer> containsRemainder = builder.computeSeries(staticAnalysisRun);
+        List<Integer> containsRemainder = builder.computeSeries(STATIC_ANALYSIS_RUN);
 
-        when(staticAnalysisRun.getTotalSize()).thenReturn(11);
+        when(STATIC_ANALYSIS_RUN.getTotalSize()).thenReturn(11);
 
-        List<Integer> containsRange = builder.computeSeries(staticAnalysisRun);
+        List<Integer> containsRange = builder.computeSeries(STATIC_ANALYSIS_RUN);
         assertThat(containsRemainder.get(1)).isEqualTo(1);
         assertThat(containsRange.get(1)).isEqualTo(5);
     }
@@ -104,82 +97,246 @@ class HealthSeriesBuilderTest {
     ///////////////////////////////////////////////////////////////////////////////
 
     @Test
-    void createEmptyDataSetWithEmptyResults() {
-        CategoryDataset emptyDataset = new DataSetBuilder<String, LocalDateLabel>().build();
+    void createDataSetWithEmptyResultsEqualsEmptyDataSet() {
+        CategoryDataset dataSet = new DataSetBuilder<String, LocalDateLabel>().build();
+        SeriesBuilder builder = new HealthSeriesBuilder(HEALTH_DESCRIPTOR);
 
         when(CONFIGURATION.useBuildDateAsDomain()).thenReturn(false, true);
 
-        assertThat(SERIES_BUILDER.createDataSet(CONFIGURATION, EMPTY_RESULTS))
-                .isEqualTo(emptyDataset);
-        assertThat(SERIES_BUILDER.createDataSet(CONFIGURATION, EMPTY_RESULTS))
-                .isEqualTo(emptyDataset);
+        assertThat(builder.createDataSet(CONFIGURATION, EMPTY_RESULTS))
+                .isEqualTo(dataSet);
+        assertThat(builder.createDataSet(CONFIGURATION, EMPTY_RESULTS))
+                .isEqualTo(dataSet);
     }
 
-
-    @Ignore
+    @Test
     void createDataSetWithSampleResults() {
+        ResultTime resultTime = mock(ResultTime.class);
+        when(resultTime.areResultsTooOld(CONFIGURATION, STATIC_ANALYSIS_RUN)).thenReturn(false);
 
-        when(CONFIGURATION.useBuildDateAsDomain()).thenReturn(false, true);
+        when(CONFIGURATION.useBuildDateAsDomain()).thenReturn(false);
+        when(CONFIGURATION.isBuildCountDefined()).thenReturn(false);
 
         when(STATIC_ANALYSIS_RUN.getBuild()).thenReturn(ANALYSIS_BUILD);
         when(STATIC_ANALYSIS_RUN_2.getBuild()).thenReturn(ANALYSIS_BUILD_2);
         when(STATIC_ANALYSIS_RUN_3.getBuild()).thenReturn(ANALYSIS_BUILD_3);
 
-        final long zeroMillis = 0L;
-        final long thousandMillis = 1_000L;
-        final long millionMillis = 1_000_000L;
+        when(STATIC_ANALYSIS_RUN.getTotalSize()).thenReturn(0);
+        when(STATIC_ANALYSIS_RUN_2.getTotalSize()).thenReturn(1);
+        when(STATIC_ANALYSIS_RUN_3.getTotalSize()).thenReturn(2);
 
-        when(ANALYSIS_BUILD.getTimeInMillis()).thenReturn(zeroMillis);
-        when(ANALYSIS_BUILD_2.getTimeInMillis()).thenReturn(thousandMillis);
-        when(ANALYSIS_BUILD_3.getTimeInMillis()).thenReturn(millionMillis);
+        when(ANALYSIS_BUILD.compareTo(ANALYSIS_BUILD_2)).thenReturn(-1);
+        when(ANALYSIS_BUILD.compareTo(ANALYSIS_BUILD_3)).thenReturn(-1);
 
-        LocalDate localDate = new LocalDate(zeroMillis);
-        LocalDate localDate1 = new LocalDate(thousandMillis);
-        LocalDate localDate2 = new LocalDate(millionMillis);
+        when(ANALYSIS_BUILD_2.compareTo(ANALYSIS_BUILD)).thenReturn(1);
+        when(ANALYSIS_BUILD_2.compareTo(ANALYSIS_BUILD_3)).thenReturn(-1);
 
+        when(ANALYSIS_BUILD_3.compareTo(ANALYSIS_BUILD)).thenReturn(1);
+        when(ANALYSIS_BUILD_3.compareTo(ANALYSIS_BUILD_2)).thenReturn(1);
 
-        CategoryDataset dataSet = SERIES_BUILDER.createDataSet(CONFIGURATION, RESULTS);
-        //assertThat(dataSet)
+        when(HEALTH_DESCRIPTOR.isEnabled()).thenReturn(false);
 
+        SeriesBuilder builder = new HealthSeriesBuilder(HEALTH_DESCRIPTOR, resultTime);
+        CategoryDataset dataSet = builder.createDataSet(CONFIGURATION, RESULTS);
 
+        int columnCount = dataSet.getColumnCount();
+        int rowCount = dataSet.getRowCount();
+
+        assertThat(columnCount).isEqualTo(3);
+        assertThat(rowCount).isEqualTo(1);
+
+        int zero = dataSet.getValue(0, 0).intValue();
+        int one = dataSet.getValue(0, 1).intValue();
+        int two = dataSet.getValue(0, 2).intValue();
+
+        assertThat(zero).isEqualTo(0);
+        assertThat(one).isEqualTo(1);
+        assertThat(two).isEqualTo(2);
     }
 
     @Test
-    void checkComputeDayDelta() {
-        Calendar calendar = mock(Calendar.class);
+    void createEmptyDataSetWithSampleResultsWhereFirstBuildIsTooOld() {
+        ResultTime resultTime = mock(ResultTime.class);
+        when(resultTime.areResultsTooOld(CONFIGURATION, STATIC_ANALYSIS_RUN)).thenReturn(true, false);
+        when(resultTime.areResultsTooOld(CONFIGURATION, STATIC_ANALYSIS_RUN_2)).thenReturn(false);
+        when(resultTime.areResultsTooOld(CONFIGURATION, STATIC_ANALYSIS_RUN_3)).thenReturn(false);
 
-        when(calendar.getTimeInMillis()).thenReturn(0L, 0L, (long) A_DAY_IN_MSEC);
+        when(CONFIGURATION.useBuildDateAsDomain()).thenReturn(false);
+        when(CONFIGURATION.isBuildCountDefined()).thenReturn(false);
+
         when(STATIC_ANALYSIS_RUN.getBuild()).thenReturn(ANALYSIS_BUILD);
-        when(ANALYSIS_BUILD.getTimeInMillis()).thenReturn(0L, (long) A_DAY_IN_MSEC);
+        when(STATIC_ANALYSIS_RUN_2.getBuild()).thenReturn(ANALYSIS_BUILD_2);
+        when(STATIC_ANALYSIS_RUN_3.getBuild()).thenReturn(ANALYSIS_BUILD_3);
 
-        assertThat(SeriesBuilder.computeDayDelta(calendar, 0L))
-                .isEqualTo(0L);
+        when(STATIC_ANALYSIS_RUN.getTotalSize()).thenReturn(0);
+        when(STATIC_ANALYSIS_RUN_2.getTotalSize()).thenReturn(1);
+        when(STATIC_ANALYSIS_RUN_3.getTotalSize()).thenReturn(2);
 
-        assertThat(SeriesBuilder.computeDayDelta(calendar, A_DAY_IN_MSEC))
-                .isEqualTo(1L);
+        SeriesBuilder builder = new HealthSeriesBuilder(HEALTH_DESCRIPTOR, resultTime);
+        CategoryDataset dataSet = builder.createDataSet(CONFIGURATION, RESULTS);
 
-        assertThat(SeriesBuilder.computeDayDelta(calendar, STATIC_ANALYSIS_RUN))
-                .isEqualTo(1L);
+        List<?> rowKeys = dataSet.getRowKeys();
+        List<?> colKeys = dataSet.getColumnKeys();
+        int rc = dataSet.getRowCount();
+        int cc = dataSet.getColumnCount();
 
-        assertThat(SeriesBuilder.computeDayDelta(calendar, STATIC_ANALYSIS_RUN))
-                .isEqualTo(0L);
+        assertThat(rc).isEqualTo(0);
+        assertThat(cc).isEqualTo(0);
+
+        assertThat(rowKeys.size()).isEqualTo(0);
+        assertThat(colKeys.size()).isEqualTo(0);
     }
 
     @Test
-    void buildIsTooOldAndNotTooOld() {
+    void createDataSetWithSampleResultsWhereConfigurationBuildCountDefinedIsTrueAndBuildCountIsTwo() {
+        ResultTime resultTime = mock(ResultTime.class);
+        when(resultTime.areResultsTooOld(CONFIGURATION, STATIC_ANALYSIS_RUN)).thenReturn(false);
+
+        when(CONFIGURATION.useBuildDateAsDomain()).thenReturn(false);
+        when(CONFIGURATION.isBuildCountDefined()).thenReturn(true);
+        when(CONFIGURATION.getBuildCount()).thenReturn(2);
+
+        when(STATIC_ANALYSIS_RUN.getBuild()).thenReturn(ANALYSIS_BUILD);
+        when(STATIC_ANALYSIS_RUN_2.getBuild()).thenReturn(ANALYSIS_BUILD_2);
+        when(STATIC_ANALYSIS_RUN_3.getBuild()).thenReturn(ANALYSIS_BUILD_3);
+
+        when(STATIC_ANALYSIS_RUN.getTotalSize()).thenReturn(0);
+        when(STATIC_ANALYSIS_RUN_2.getTotalSize()).thenReturn(1);
+        when(STATIC_ANALYSIS_RUN_3.getTotalSize()).thenReturn(2);
+
+        when(ANALYSIS_BUILD.compareTo(ANALYSIS_BUILD_2)).thenReturn(-1);
+        when(ANALYSIS_BUILD.compareTo(ANALYSIS_BUILD_3)).thenReturn(-1);
+
+        when(ANALYSIS_BUILD_2.compareTo(ANALYSIS_BUILD)).thenReturn(1);
+        when(ANALYSIS_BUILD_2.compareTo(ANALYSIS_BUILD_3)).thenReturn(-1);
+
+        when(ANALYSIS_BUILD_3.compareTo(ANALYSIS_BUILD)).thenReturn(1);
+        when(ANALYSIS_BUILD_3.compareTo(ANALYSIS_BUILD_2)).thenReturn(1);
+
+        when(HEALTH_DESCRIPTOR.isEnabled()).thenReturn(false);
+
+        SeriesBuilder builder = new HealthSeriesBuilder(HEALTH_DESCRIPTOR, resultTime);
+        CategoryDataset dataSet = builder.createDataSet(CONFIGURATION, RESULTS);
+
+        List<?> rowKeys = dataSet.getRowKeys();
+        List<?> colKeys = dataSet.getColumnKeys();
+
+        assertThat(rowKeys.size()).isEqualTo(1);
+        assertThat(colKeys.size()).isEqualTo(2);
+
+        int zero = dataSet.getValue(0, 0).intValue();
+        int one = dataSet.getValue(0, 1).intValue();
+
+        assertThat(zero).isEqualTo(0);
+        assertThat(one).isEqualTo(1);
+        assertThatThrownBy(() -> dataSet.getValue(0, 2)).isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    void createDataSetWithSampleResultsWhereConfigurationUseBuildDateAsDomainIsTrue() {
+        SeriesBuilder builder = new HealthSeriesBuilder(HEALTH_DESCRIPTOR);
+
+        when(CONFIGURATION.useBuildDateAsDomain()).thenReturn(true);
+
+        when(STATIC_ANALYSIS_RUN.getBuild()).thenReturn(ANALYSIS_BUILD);
+        when(STATIC_ANALYSIS_RUN_2.getBuild()).thenReturn(ANALYSIS_BUILD_2);
+        when(STATIC_ANALYSIS_RUN_3.getBuild()).thenReturn(ANALYSIS_BUILD_3);
+
+        when(STATIC_ANALYSIS_RUN.getTotalSize()).thenReturn(0);
+        when(STATIC_ANALYSIS_RUN_2.getTotalSize()).thenReturn(1);
+        when(STATIC_ANALYSIS_RUN_3.getTotalSize()).thenReturn(2);
+
+        when(ANALYSIS_BUILD.compareTo(ANALYSIS_BUILD_2)).thenReturn(-1);
+        when(ANALYSIS_BUILD.compareTo(ANALYSIS_BUILD_3)).thenReturn(-1);
+
+        when(ANALYSIS_BUILD_2.compareTo(ANALYSIS_BUILD)).thenReturn(1);
+        when(ANALYSIS_BUILD_2.compareTo(ANALYSIS_BUILD_3)).thenReturn(-1);
+
+        when(ANALYSIS_BUILD_3.compareTo(ANALYSIS_BUILD)).thenReturn(1);
+        when(ANALYSIS_BUILD_3.compareTo(ANALYSIS_BUILD_2)).thenReturn(1);
+
+        CategoryDataset dataSet = builder.createDataSet(CONFIGURATION, RESULTS);
+
+        List<?> rowKeys = dataSet.getRowKeys();
+        List<?> colKeys = dataSet.getColumnKeys();
+
+        assertThat(rowKeys.size()).isEqualTo(3);
+        assertThat(colKeys.size()).isEqualTo(1);
+
+        int one = dataSet.getValue(0, 0).intValue();
+        int zero1 = dataSet.getValue(1, 0).intValue();
+        int zero2 = dataSet.getValue(2, 0).intValue();
+
+        assertThat(one).isEqualTo(1);
+        assertThat(zero1).isEqualTo(0);
+        assertThat(zero2).isEqualTo(0);
+    }
+
+    @Test
+    void createAggregationWithThreeResultActions() {
         GraphConfiguration configuration = mock(GraphConfiguration.class);
-        when(configuration.isDayCountDefined()).thenReturn(true, false);
-        when(configuration.getDayCount()).thenReturn(0, 1, Integer.MAX_VALUE);
-        when(STATIC_ANALYSIS_RUN.getBuild()).thenReturn(ANALYSIS_BUILD);
-        when(ANALYSIS_BUILD.getTimeInMillis()).thenReturn((long) A_DAY_IN_MSEC, Long.MIN_VALUE);
 
-        assertThat(SeriesBuilder.isBuildTooOld(configuration, STATIC_ANALYSIS_RUN))
-                .isTrue();
+        ResultHistory resultHistory = mock(ResultHistory.class);
+        ResultHistory resultHistory2 = mock(ResultHistory.class);
+        ResultHistory resultHistory3 = mock(ResultHistory.class);
 
-        assertThat(SeriesBuilder.isBuildTooOld(configuration, STATIC_ANALYSIS_RUN))
-                .isFalse();
+        AnalysisResult analysisResult = mock(AnalysisResult.class);
+        AnalysisResult analysisResult2 = mock(AnalysisResult.class);
+        AnalysisResult analysisResult3 = mock(AnalysisResult.class);
 
-        assertThat(SeriesBuilder.isBuildTooOld(configuration, STATIC_ANALYSIS_RUN))
-                .isFalse();
+        when(resultHistory.getBaseline()).thenReturn(Optional.of(analysisResult));
+        when(resultHistory2.getBaseline()).thenReturn(Optional.of(analysisResult2));
+        when(resultHistory3.getBaseline()).thenReturn(Optional.of(analysisResult3));
+
+        when(resultHistory.getPreviousResult()).thenReturn(Optional.of(analysisResult));
+        when(resultHistory2.getPreviousResult()).thenReturn(Optional.of(analysisResult2));
+        when(resultHistory3.getPreviousResult()).thenReturn(Optional.of(analysisResult3));
+
+        when(resultHistory.iterator()).thenReturn(Collections.singletonList(analysisResult).iterator());
+        when(resultHistory2.iterator()).thenReturn(Collections.singletonList(analysisResult2).iterator());
+        when(resultHistory3.iterator()).thenReturn(Collections.singletonList(analysisResult3).iterator());
+
+        when(analysisResult.getBuild()).thenReturn(ANALYSIS_BUILD);
+        when(analysisResult2.getBuild()).thenReturn(ANALYSIS_BUILD_2);
+        when(analysisResult3.getBuild()).thenReturn(ANALYSIS_BUILD_3);
+
+        when(ANALYSIS_BUILD.getTimeInMillis()).thenReturn(1L);
+        when(ANALYSIS_BUILD_2.getTimeInMillis()).thenReturn(1_000L);
+        when(ANALYSIS_BUILD_3.getTimeInMillis()).thenReturn(1_000_000L);
+
+        Collection<ResultHistory> resultActions = Arrays.asList(resultHistory, resultHistory2, resultHistory2);
+
+        ResultTime resultTime = mock(ResultTime.class);
+        when(resultTime.areResultsTooOld(configuration, analysisResult)).thenReturn(false);
+        when(resultTime.areResultsTooOld(configuration, analysisResult2)).thenReturn(false);
+        when(resultTime.areResultsTooOld(configuration, analysisResult3)).thenReturn(false);
+
+        SeriesBuilder seriesBuilder = new HealthSeriesBuilder(HEALTH_DESCRIPTOR, resultTime);
+        CategoryDataset dataset = seriesBuilder.createAggregation(configuration, resultActions);
+
+        List<?> rowKeys = dataset.getRowKeys();
+        List<?> colKeys = dataset.getRowKeys();
+
+        assertThat(rowKeys.size()).isEqualTo(1);
+        assertThat(colKeys.size()).isEqualTo(1);
+
+        int zero = dataset.getValue(0, 0).intValue();
+
+        assertThat(zero).isEqualTo(0);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
