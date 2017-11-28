446db5b7fc3c90b5e2dc0bdbd954c87f757ffb8a
Tom <weissbier.weissbier@googlemail.com>
2017-11-28 11:03:02 +0100

    Add tests for the QualityGate and for the QualityGateBuilder

--- src/main/java/io/jenkins/plugins/analysis/core/quality/QualityGate.java
+++ src/main/java/io/jenkins/plugins/analysis/core/quality/QualityGate.java
@@ -6,40 +6,177 @@ package io.jenkins.plugins.analysis.core.quality;
  * @author Ullrich Hafner
  */
 public class QualityGate {
-    private final int failureThreshold;
 
-    /**
-     * Creates a new instance of {@link QualityGate}.
-     *
-     * @param failureThreshold
-     *         the number of issues that will fail a build
-     */
-    public QualityGate(final int failureThreshold) {
-        this.failureThreshold = failureThreshold;
+    private int totalUnstableAllPriorities;
+    private int totalUnstableHighPriority;
+    private int totalUnstableNormalPriority;
+    private int totalUnstableLowPriority;
+
+    private int totalFailedAllPriorities;
+    private int totalFailedHighPriority;
+    private int totalFailedNormalPriority;
+    private int totalFailedLowPriority;
+
+    private int newUnstableAllPriorities;
+    private int newUnstableHighPriority;
+    private int newUnstableNormalPriority;
+    private int newUnstableLowPriority;
+
+    private int newFailedAllPriorities;
+    private int newFailedHighPriority;
+    private int newFailedNormalPriority;
+    private int newFailedLowPriority;
+
+    public QualityGate(final int totalUnstableAllPriorities, final int totalUnstableHighPriority, final int totalUnstableNormalPriority,
+            final int totalUnstableLowPriority, final int totalFailedAllPriorities, final int totalFailedHighPriority, final int totalFailedNormalPriority,
+            final int totalFailedLowPriority, final int newUnstableAllPriorities, final int newUnstableHighPriority, final int newUnstableNormalPriority,
+            final int newUnstableLowPriority, final int newFailedAllPriorities, final int newFailedHighPriority, final int newFailedNormalPriority, final int newFailedLowPriority) {
+        this.totalUnstableAllPriorities = totalUnstableAllPriorities;
+        this.totalUnstableHighPriority = totalUnstableHighPriority;
+        this.totalUnstableNormalPriority = totalUnstableNormalPriority;
+        this.totalUnstableLowPriority = totalUnstableLowPriority;
+        this.totalFailedAllPriorities = totalFailedAllPriorities;
+        this.totalFailedHighPriority = totalFailedHighPriority;
+        this.totalFailedNormalPriority = totalFailedNormalPriority;
+        this.totalFailedLowPriority = totalFailedLowPriority;
+        this.newUnstableAllPriorities = newUnstableAllPriorities;
+        this.newUnstableHighPriority = newUnstableHighPriority;
+        this.newUnstableNormalPriority = newUnstableNormalPriority;
+        this.newUnstableLowPriority = newUnstableLowPriority;
+        this.newFailedAllPriorities = newFailedAllPriorities;
+        this.newFailedHighPriority = newFailedHighPriority;
+        this.newFailedNormalPriority = newFailedNormalPriority;
+        this.newFailedLowPriority = newFailedLowPriority;
     }
 
-    /**
-     * Creates a new instance of {@link QualityGate}. No thresholds are set.
-     */
-    public QualityGate() {
-        this(0);
+
+
+    public int getTotalUnstableAllPriorities() {
+        return totalUnstableAllPriorities;
     }
 
-    /**
-     * Determines if a failure threshold for the total number of issues is set.
-     *
-     * @return {@code true} if the failure threshold for the total number of issues is set
-     */
-    public boolean hasFailureThreshold() {
-        return failureThreshold > 0;
+    public int getTotalUnstableHighPriority() {
+        return totalUnstableHighPriority;
     }
 
-    /**
-     * Returns the failure threshold for the total number of issues.
-     *
-     * @return the failure threshold for the total number of issues
-     */
-    public int getFailureThreshold() {
-        return failureThreshold;
+    public int getTotalUnstableNormalPriority() {
+        return totalUnstableNormalPriority;
     }
+
+    public int getTotalUnstableLowPriority() {
+        return totalUnstableLowPriority;
+    }
+
+    public int getTotalFailedAllPriorities() {
+        return totalFailedAllPriorities;
+    }
+
+    public int getTotalFailedHighPriority() {
+        return totalFailedHighPriority;
+    }
+
+    public int getTotalFailedNormalPriority() {
+        return totalFailedNormalPriority;
+    }
+
+    public int getTotalFailedLowPriority() {
+        return totalFailedLowPriority;
+    }
+
+    public int getNewUnstableAllPriorities() {
+        return newUnstableAllPriorities;
+    }
+
+    public int getNewUnstableHighPriority() {
+        return newUnstableHighPriority;
+    }
+
+    public int getNewUnstableNormalPriority() {
+        return newUnstableNormalPriority;
+    }
+
+    public int getNewUnstableLowPriority() {
+        return newUnstableLowPriority;
+    }
+
+    public int getNewFailedAllPriorities() {
+        return newFailedAllPriorities;
+    }
+
+    public int getNewFailedHighPriority() {
+        return newFailedHighPriority;
+    }
+
+    public int getNewFailedNormalPriority() {
+        return newFailedNormalPriority;
+    }
+
+    public int getNewFailedLowPriority() {
+        return newFailedLowPriority;
+    }
+
+    public boolean isTotalUnstableAllPriorities() {
+        return totalUnstableAllPriorities >= 0;
+    }
+
+    public boolean isTotalUnstableHighPriority() {
+        return totalUnstableHighPriority >= 0;
+    }
+
+    public boolean isTotalUnstableNormalPriority() {
+        return totalUnstableNormalPriority >= 0;
+    }
+
+    public boolean isTotalUnstableLowPriority() {
+        return totalUnstableLowPriority >= 0;
+    }
+
+    public boolean isTotalFailedAllPriorities() {
+        return totalFailedAllPriorities >= 0;
+    }
+
+    public boolean isTotalFailedHighPriority() {
+        return totalFailedHighPriority >= 0;
+    }
+
+    public boolean isTotalFailedNormalPriority() {
+        return totalFailedNormalPriority >= 0;
+    }
+
+    public boolean isTotalFailedLowPriority() {
+        return totalFailedLowPriority >= 0;
+    }
+
+    public boolean isNewUnstableAllPriorities() {
+        return newUnstableAllPriorities >= 0;
+    }
+
+    public boolean isNewUnstableHighPriority() {
+        return newUnstableHighPriority >= 0;
+    }
+
+    public boolean isNewUnstableNormalPriority() {
+        return newUnstableNormalPriority >= 0;
+    }
+
+    public boolean isNewUnstableLowPriority() {
+        return newUnstableLowPriority >= 0;
+    }
+
+    public boolean isNewFailedAllPriorities() {
+        return newFailedAllPriorities >= 0;
+    }
+
+    public boolean isNewFailedHighPriority() {
+        return newFailedHighPriority >= 0;
+    }
+
+    public boolean isNewFailedNormalPriority() {
+        return newFailedNormalPriority >= 0;
+    }
+
+    public boolean isNewFailedLowPriority() {
+        return newFailedLowPriority >= 0;
+    }
+
 }
---
+++ src/test/java/io/jenkins/plugins/analysis/core/quality/QualityGateAssert.java
@@ -0,0 +1,307 @@
+package io.jenkins.plugins.analysis.core.quality;
+
+import org.assertj.core.api.AbstractAssert;
+
+public class QualityGateAssert extends AbstractAssert<QualityGateAssert, QualityGate> {
+
+    private static final String EXPECTED_BUT_WAS_MESSAGE = "Expected to be <%s> but was <%s>.";
+
+    public QualityGateAssert(final QualityGate qualityGate) {
+        super(qualityGate, QualityGateAssert.class);
+    }
+
+    public static QualityGateAssert assertThat(final QualityGate actual) {
+        return new QualityGateAssert(actual);
+    }
+
+    public QualityGateAssert hasTotalFailedAllPriorities(final int totalFailedAllPriorities) {
+        isNotNull();
+
+        if (actual.getTotalFailedAllPriorities() != totalFailedAllPriorities) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, totalFailedAllPriorities, actual.getTotalFailedAllPriorities());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasTotalFailedHighPriority(final int totalFailedHighPriority) {
+        isNotNull();
+
+        if (actual.getTotalFailedHighPriority() != totalFailedHighPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, totalFailedHighPriority, actual.getTotalFailedHighPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasTotalFailedNormalPriority(final int totalFailedNormalPriority) {
+        isNotNull();
+
+        if (actual.getTotalFailedNormalPriority() != totalFailedNormalPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, totalFailedNormalPriority, actual.getTotalFailedNormalPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasTotalFailedLowPriority(final int totalFailedLowPriority) {
+        isNotNull();
+
+        if (actual.getTotalFailedLowPriority() != totalFailedLowPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, totalFailedLowPriority, actual.getTotalFailedLowPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasTotalUnstableAllPriorities(final int totalUnstableAllPriorities) {
+        isNotNull();
+
+        if (actual.getTotalUnstableAllPriorities() != totalUnstableAllPriorities) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, totalUnstableAllPriorities, actual.getTotalUnstableAllPriorities());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasTotalUnstableHighPriority(final int totalUnstableHighPriority) {
+        isNotNull();
+
+        if (actual.getTotalUnstableHighPriority() != totalUnstableHighPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, totalUnstableHighPriority, actual.getTotalUnstableHighPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasTotalUnstableNormalPriority(final int totalUnstableNormalPriority) {
+        isNotNull();
+
+        if (actual.getTotalUnstableNormalPriority() != totalUnstableNormalPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, totalUnstableNormalPriority, actual.getTotalUnstableNormalPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasTotalUnstableLowPriority(final int totalUnstableLowPriority) {
+        isNotNull();
+
+        if (actual.getTotalUnstableLowPriority() != totalUnstableLowPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, totalUnstableLowPriority, actual.getTotalUnstableLowPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasNewFailedAllPriorities(final int newFailedAllPriorities) {
+        isNotNull();
+
+        if (actual.getNewFailedAllPriorities() != newFailedAllPriorities) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, newFailedAllPriorities, actual.getNewFailedAllPriorities());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasNewFailedHighPriority(final int newFailedHighPriority) {
+        isNotNull();
+
+        if (actual.getNewFailedHighPriority() != newFailedHighPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, newFailedHighPriority, actual.getNewFailedHighPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasNewFailedNormalPriority(final int newFailedNormalPriority) {
+        isNotNull();
+
+        if (actual.getNewFailedNormalPriority() != newFailedNormalPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, newFailedNormalPriority, actual.getNewFailedNormalPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasNewFailedLowPriority(final int newFailedLowPriority) {
+        isNotNull();
+
+        if (actual.getNewFailedLowPriority() != newFailedLowPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, newFailedLowPriority, actual.getNewFailedLowPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasNewUnstableAllPriorities(final int newUnstableAllPriorities) {
+        isNotNull();
+
+        if (actual.getNewUnstableAllPriorities() != newUnstableAllPriorities) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, newUnstableAllPriorities, actual.getNewUnstableAllPriorities());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasNewUnstableHighPriority(final int newUnstableHighPriority) {
+        isNotNull();
+
+        if (actual.getNewUnstableHighPriority() != newUnstableHighPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, newUnstableHighPriority, actual.getNewUnstableHighPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasNewUnstableNormalPriority(final int newUnstableNormalPriority) {
+        isNotNull();
+
+        if (actual.getNewUnstableNormalPriority() != newUnstableNormalPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, newUnstableNormalPriority, actual.getNewUnstableNormalPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasNewUnstableLowPriority(final int newUnstableLowPriority) {
+        isNotNull();
+
+        if (actual.getNewUnstableLowPriority() != newUnstableLowPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, newUnstableLowPriority, actual.getNewUnstableLowPriority());
+        }
+        return this;
+    }
+
+
+    public QualityGateAssert hasIsTotalFailedAllPriorities(final boolean isTotalFailedAllPriorities) {
+        isNotNull();
+
+        if (actual.isTotalFailedAllPriorities() != isTotalFailedAllPriorities) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, isTotalFailedAllPriorities, actual.isTotalFailedAllPriorities());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasIsTotalFailedHighPriority(final boolean isTotalFailedHighPriority) {
+        isNotNull();
+
+        if (actual.isTotalFailedHighPriority() != isTotalFailedHighPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, isTotalFailedHighPriority, actual.isTotalFailedHighPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasIsTotalFailedNormalPriority(final boolean isTotalFailedNormalPriority) {
+        isNotNull();
+
+        if (actual.isTotalFailedNormalPriority() != isTotalFailedNormalPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, isTotalFailedNormalPriority, actual.isTotalFailedNormalPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasIsTotalFailedLowPriority(final boolean isTotalFailedLowPriority) {
+        isNotNull();
+
+        if (actual.isTotalFailedLowPriority() != isTotalFailedLowPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, isTotalFailedLowPriority, actual.isTotalFailedLowPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasIsTotalUnstableAllPriorities(final boolean isTotalUnstableAllPriorities) {
+        isNotNull();
+
+        if (actual.isTotalUnstableAllPriorities() != isTotalUnstableAllPriorities) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, isTotalUnstableAllPriorities, actual.isTotalUnstableAllPriorities());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasIsTotalUnstableHighPriority(final boolean isTotalUnstableHighPriority) {
+        isNotNull();
+
+        if (actual.isTotalUnstableHighPriority() != isTotalUnstableHighPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, isTotalUnstableHighPriority, actual.isTotalUnstableHighPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasIsTotalUnstableNormalPriority(final boolean isTotalUnstableNormalPriority) {
+        isNotNull();
+
+        if (actual.isTotalUnstableNormalPriority() != isTotalUnstableNormalPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, isTotalUnstableNormalPriority, actual.isTotalUnstableNormalPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasIsTotalUnstableLowPriority(final boolean isTotalUnstableLowPriority) {
+        isNotNull();
+
+        if (actual.isTotalUnstableLowPriority() != isTotalUnstableLowPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, isTotalUnstableLowPriority, actual.isTotalUnstableLowPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasIsNewFailedAllPriorities(final boolean isNewFailedAllPriorities) {
+        isNotNull();
+
+        if (actual.isNewFailedAllPriorities() != isNewFailedAllPriorities) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, isNewFailedAllPriorities, actual.isNewFailedAllPriorities());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasIsNewFailedHighPriority(final boolean isNewFailedHighPriority) {
+        isNotNull();
+
+        if (actual.isNewFailedHighPriority() != isNewFailedHighPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, isNewFailedHighPriority, actual.isNewFailedHighPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasIsNewFailedNormalPriority(final boolean isNewFailedNormalPriority) {
+        isNotNull();
+
+        if (actual.isNewFailedNormalPriority() != isNewFailedNormalPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, isNewFailedNormalPriority, actual.isNewFailedNormalPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasIsNewFailedLowPriority(final boolean isNewFailedLowPriority) {
+        isNotNull();
+
+        if (actual.isNewFailedLowPriority() != isNewFailedLowPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, isNewFailedLowPriority, actual.isNewFailedLowPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasIsNewUnstableAllPriorities(final boolean isNewUnstableAllPriorities) {
+        isNotNull();
+
+        if (actual.isNewUnstableAllPriorities() != isNewUnstableAllPriorities) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, isNewUnstableAllPriorities, actual.isNewUnstableAllPriorities());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasIsNewUnstableHighPriority(final boolean isNewUnstableHighPriority) {
+        isNotNull();
+
+        if (actual.isNewUnstableHighPriority() != isNewUnstableHighPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, isNewUnstableHighPriority, actual.isNewUnstableHighPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasIsNewUnstableNormalPriority(final boolean isNewUnstableNormalPriority) {
+        isNotNull();
+
+        if (actual.isNewUnstableNormalPriority() != isNewUnstableNormalPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, isNewUnstableNormalPriority, actual.isNewUnstableNormalPriority());
+        }
+        return this;
+    }
+
+    public QualityGateAssert hasIsNewUnstableLowPriority(final boolean isNewUnstableLowPriority) {
+        isNotNull();
+
+        if (actual.isNewUnstableLowPriority() != isNewUnstableLowPriority) {
+            failWithMessage(EXPECTED_BUT_WAS_MESSAGE, isNewUnstableLowPriority, actual.isNewUnstableLowPriority());
+        }
+        return this;
+    }
+
+
+}
