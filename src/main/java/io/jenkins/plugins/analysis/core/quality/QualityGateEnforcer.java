44e975b2046cf2cec62df08f15194b0bb48e7773
J. Behrmann <behrmann@hm.edu>
2017-11-29 23:44:14 +0100

    QualityGateEnforcer now enforces thresholds according to spec

--- src/main/java/io/jenkins/plugins/analysis/core/quality/QualityGateEnforcer.java
+++ src/main/java/io/jenkins/plugins/analysis/core/quality/QualityGateEnforcer.java
@@ -5,13 +5,49 @@ import hudson.model.Result;
 /**
  * Enforces the defined quality gates.
  *
- * @author Ullrich Hafner
+ * @author Joscha Behrmann
  */
 public class QualityGateEnforcer {
+
+    /**
+     * Evaluates the quality of the run.
+     *
+     * @param run the {@link StaticAnalysisRun} to evaluate
+     * @param qualityGate the {@link QualityGate} to evaluate by
+     * @return {@link Result#FAILURE} if the run exceeded the qualityGates 'failure' criteria
+     *         {@link Result#UNSTABLE} if the run exceeded the the qualityGates 'unstable' criteria
+     *         {@link Result#SUCCESS} if the run passed the qualityGate
+     */
     public Result evaluate(final StaticAnalysisRun run, final QualityGate qualityGate) {
         if (qualityGate.hasFailureThreshold()) {
-            if (run.getTotalHighPrioritySize() >= qualityGate.getFailureThreshold()) {
+            if (run.getTotalSize() > qualityGate.getTotalPriorityAllFailed()
+                    || run.getTotalHighPrioritySize() > qualityGate.getTotalPriorityHighFailed()
+                    || run.getTotalNormalPrioritySize() > qualityGate.getTotalPriorityNormalFailed()
+                    || run.getTotalLowPrioritySize() > qualityGate.getTotalPriorityLowFailed()) {
                 return Result.FAILURE;
+            }
+
+            if (run.getTotalSize() > qualityGate.getTotalPriorityAllUnstable()
+                    || run.getTotalHighPrioritySize() > qualityGate.getTotalPriorityHighUnstable()
+                    || run.getTotalNormalPrioritySize() > qualityGate.getTotalPriorityNormalUnstable()
+                    || run.getTotalLowPrioritySize() > qualityGate.getTotalPriorityLowUnstable()) {
+                return Result.UNSTABLE;
+            }
+
+            if (qualityGate.shouldComputeNewWarnings()) {
+                if (run.getNewSize() > qualityGate.getNewPriorityAllFailed()
+                        || run.getNewHighPrioritySize() > qualityGate.getNewPriorityHighFailed()
+                        || run.getNewNormalPrioritySize() > qualityGate.getNewPriorityNormalFailed()
+                        || run.getNewLowPrioritySize() > qualityGate.getNewPriorityLowFailed()) {
+                    return Result.FAILURE;
+                }
+
+                if (run.getNewSize() > qualityGate.getNewPriorityAllUnstable()
+                        || run.getNewHighPrioritySize() > qualityGate.getNewPriorityHighUnstable()
+                        || run.getNewNormalPrioritySize() > qualityGate.getNewPriorityNormalUnstable()
+                        || run.getNewLowPrioritySize() > qualityGate.getNewPriorityLowUnstable()) {
+                    return Result.UNSTABLE;
+                }
             }
         }
         return Result.SUCCESS;
