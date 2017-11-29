e9ea5f5d99197527ed57e527e93eb5119e776048
JustABunchOfCode <raphael@familie-furch.de>
2017-11-29 11:08:44 +0100

    Added QualityGate + Builder + Tests Added ThresholdSet

--- src/main/java/io/jenkins/plugins/analysis/core/quality/QualityGate.java
+++ src/main/java/io/jenkins/plugins/analysis/core/quality/QualityGate.java
@@ -5,17 +5,35 @@ import hudson.model.Result;
 /**
  * Defines quality gates for a static analysis run.
  *
- * @author Ullrich Hafner
+ * @author Raphael Furch
  */
 public class QualityGate {
 
+    /**
+     * Unstable threshold for total Builds .
+     */
     private final ThresholdSet totalBuildsUnstableThresholds;
+    /**
+     * Failure threshold for total Builds .
+     */
     private final ThresholdSet totalBuildsFailureThresholds;
+    /**
+     * Unstable threshold for new Builds .
+     */
     private final ThresholdSet newBuildsUnstableThresholds;
+    /**
+     * Failure threshold for new Builds .
+     */
     private final ThresholdSet newBuildsFailureThresholds;
 
 
-
+    /**
+     * C.
+     * @param totalBuildsUnstableThresholds = Unstable threshold for total Builds .
+     * @param totalBuildsFailureThresholds = Failure threshold for total Builds .
+     * @param newBuildsUnstableThresholds = Unstable threshold for new Builds .
+     * @param newBuildsFailureThresholds = Failure threshold for new Builds .
+     */
     QualityGate(ThresholdSet totalBuildsUnstableThresholds, ThresholdSet totalBuildsFailureThresholds, ThresholdSet newBuildsUnstableThresholds, ThresholdSet newBuildsFailureThresholds) {
         this.totalBuildsUnstableThresholds = totalBuildsUnstableThresholds;
         this.totalBuildsFailureThresholds = totalBuildsFailureThresholds;
@@ -23,27 +41,37 @@ public class QualityGate {
         this.newBuildsFailureThresholds = newBuildsFailureThresholds;
     }
 
+    /**
+     * Evaluate a StaticAnalysisRun.
+     * If a Threshold is exceeded the result will show it.
+     * IF no Threshold is exceeded the result will be SUCCESS.
+     * @param run = StaticAnalysisRun to check.
+     * @return the result of the evaluation.
+     */
     public Result evaluate(final StaticAnalysisRun run) {
+        // Extract total values.
         final int totalSize = run.getTotalSize();
         final int totalHighPriority = run.getTotalHighPrioritySize();
         final int totalNormalPriority = run.getTotalNormalPrioritySize();
         final int totalLowPriority = run.getTotalLowPrioritySize();
 
+        // Extract new values
         final int newSize = run.getNewSize();
         final int newHighPriority = run.getNewHighPrioritySize();
         final int newNormalPriority = run.getNewNormalPrioritySize();
         final int newLowPriority = run.getNewLowPrioritySize();
 
-        if(totalBuildsFailureThresholds.exceedAThreshold(totalSize, totalHighPriority, totalNormalPriority, totalLowPriority)){
+        // Check if values exceeding the thresholds
+        if(totalBuildsFailureThresholds.exceedAnyThreshold(totalSize, totalHighPriority, totalNormalPriority, totalLowPriority)){
             return Result.FAILURE;
         }
-        else if(newBuildsFailureThresholds.exceedAThreshold(newSize, newHighPriority, newNormalPriority, newLowPriority)){
+        else if(newBuildsFailureThresholds.exceedAnyThreshold(newSize, newHighPriority, newNormalPriority, newLowPriority)){
             return Result.FAILURE;
         }
-        else if(totalBuildsUnstableThresholds.exceedAThreshold(totalSize, totalHighPriority, totalNormalPriority, totalLowPriority)){
+        else if(totalBuildsUnstableThresholds.exceedAnyThreshold(totalSize, totalHighPriority, totalNormalPriority, totalLowPriority)){
             return Result.UNSTABLE;
         }
-        else if(newBuildsUnstableThresholds.exceedAThreshold(newSize, newHighPriority, newNormalPriority, newLowPriority)){
+        else if(newBuildsUnstableThresholds.exceedAnyThreshold(newSize, newHighPriority, newNormalPriority, newLowPriority)){
             return Result.UNSTABLE;
         }
         else{
--- src/main/java/io/jenkins/plugins/analysis/core/quality/ThresholdSet.java
+++ src/main/java/io/jenkins/plugins/analysis/core/quality/ThresholdSet.java
@@ -1,11 +1,37 @@
 package io.jenkins.plugins.analysis.core.quality;
 
+/**
+ * A set of 4 Thresholds.
+ * One for all high, normal and low priorities.
+ *
+ * @author Raphael Furch
+ */
 public class ThresholdSet{
+    /**
+     * Threshold for all priorities.
+     */
     private final int allPrioritiesThreshold;
+    /**
+     * Threshold for high priority.
+     */
     private final int highPriorityThreshold;
+    /**
+     * Threshold for normal priority.
+     */
     private final int normalPriorityThreshold;
+    /**
+     * Threshold for low priority.
+     */
     private final int lowPriorityThreshold;
 
+    /**
+     * C.
+     * If a values is negative, the threshold will be ignored.
+     * @param allPrioritiesThreshold =  Threshold for all priorities.
+     * @param highPriorityThreshold =  Threshold for high priority.
+     * @param normalPriorityThreshold =  Threshold for normal priority.
+     * @param lowPriorityThreshold =  Threshold for low priority.
+     */
     public ThresholdSet(final int allPrioritiesThreshold, final int highPriorityThreshold, final int normalPriorityThreshold, final int lowPriorityThreshold){
         this.allPrioritiesThreshold = allPrioritiesThreshold;
         this.highPriorityThreshold = highPriorityThreshold;
@@ -13,19 +39,50 @@ public class ThresholdSet{
         this.lowPriorityThreshold = lowPriorityThreshold;
     }
 
+    /**
+     * Check if current is smaller then allThreshold.
+     * @param current = value to check.
+     * @return exceeds threshold.
+     */
     public boolean exceedAllPrioritiesThreshold(final int current){
         return this.allPrioritiesThreshold > 0 && current > allPrioritiesThreshold;
     }
+
+    /**
+     * Check if current is smaller then highThreshold.
+     * @param current = value to check.
+     * @return exceeds threshold.
+     */
     public boolean exceedHighPriorityThreshold(final int current){
         return this.highPriorityThreshold > 0 && current > highPriorityThreshold;
     }
+    /**
+     * Check if current is smaller then normalThreshold.
+     * @param current = value to check.
+     * @return exceeds threshold.
+     */
     public boolean exceedNormalPriorityThreshold(final int current){
         return this.normalPriorityThreshold > 0 && current > normalPriorityThreshold;
     }
+
+    /**
+     * Check if current is smaller then lowThreshold.
+     * @param current = value to check.
+     * @return exceeds threshold.
+     */
     public boolean exceedLowPriorityThreshold(final int current){
         return this.lowPriorityThreshold > 0 && current > lowPriorityThreshold;
     }
-    public boolean exceedAThreshold(final int all, final int high, final int normal, final int low){
+
+    /**
+     * Check if any threshold is exceeded.
+     * @param all = value to check.
+     * @param high = value to check.
+     * @param normal = value to check.
+     * @param low = value to check.
+     * @return a value exceeds its threshold.
+     */
+    public boolean exceedAnyThreshold(final int all, final int high, final int normal, final int low){
         return exceedAllPrioritiesThreshold(all)
                 || exceedHighPriorityThreshold(high)
                 || exceedNormalPriorityThreshold(normal)
