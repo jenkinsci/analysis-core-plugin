4fd1f62f53bae05765033909961c40b7a4615b9e
brumhardS <brumhard@hm.edu>
2017-11-30 16:28:36 +0100

    Add QualityGate, QualityGateBuilder, QualityGateEnforcer, QualityGateEnforcerTest

--- src/main/java/io/jenkins/plugins/analysis/core/quality/QualityGate.java
+++ src/main/java/io/jenkins/plugins/analysis/core/quality/QualityGate.java
@@ -1,45 +1,96 @@
 package io.jenkins.plugins.analysis.core.quality;
 
+import java.util.List;
+import java.util.Map;
+
+import jnr.constants.platform.PRIO;
+
+import hudson.plugins.analysis.util.model.Priority;
+import java.io.Serializable;
+
 /**
  * Defines quality gates for a static analysis run.
  *
  * @author Ullrich Hafner
  */
-public class QualityGate {
-    private final int failureThreshold;
+public class QualityGate implements Serializable{
+    private final int lowPriorityUnstable;
+    private final int lowPriorityFailure;
+    private final int normalPriorityUnstable;
+    private final int normalPriorityFailure;
+    private final int highPriorityUnstable;
+    private final int highPriorityFailure;
 
-    /**
-     * Creates a new instance of {@link QualityGate}.
-     *
-     * @param failureThreshold
-     *         the number of issues that will fail a build
-     */
-    public QualityGate(final int failureThreshold) {
-        this.failureThreshold = failureThreshold;
+    QualityGate (final int lowPriorityUnstable, final int lowPriorityFailure,
+            final int normalPriorityUnstable, final int normalPriorityFailure,
+            final int highPriorityUnstable, final int highPriorityFailure){
+        this.lowPriorityUnstable = lowPriorityUnstable;
+        this.lowPriorityFailure = lowPriorityFailure;
+        this.normalPriorityUnstable = normalPriorityUnstable;
+        this.normalPriorityFailure = normalPriorityFailure;
+        this.highPriorityUnstable = highPriorityUnstable;
+        this.highPriorityFailure = highPriorityFailure;
+
+        thresholdsAreValid();
     }
 
-    /**
-     * Creates a new instance of {@link QualityGate}. No thresholds are set.
-     */
-    public QualityGate() {
-        this(0);
+    QualityGate(){
+        this(0,0,0,0,0,0);
     }
 
-    /**
-     * Determines if a failure threshold for the total number of issues is set.
-     *
-     * @return {@code true} if the failure threshold for the total number of issues is set
-     */
-    public boolean hasFailureThreshold() {
-        return failureThreshold > 0;
+    public int getLowPriorityUnstable() {
+        return lowPriorityUnstable;
     }
 
-    /**
-     * Returns the failure threshold for the total number of issues.
-     *
-     * @return the failure threshold for the total number of issues
-     */
-    public int getFailureThreshold() {
-        return failureThreshold;
+    public int getLowPriorityFailure() {
+        return lowPriorityFailure;
+    }
+
+    public int getNormalPriorityUnstable() {
+        return normalPriorityUnstable;
+    }
+
+    public int getNormalPriorityFailure() {
+        return normalPriorityFailure;
+    }
+
+    public int getHighPriorityUnstable() {
+        return highPriorityUnstable;
+    }
+
+    public int getHighPriorityFailure() {
+        return highPriorityFailure;
+    }
+
+    private void thresholdsAreValid() throws IllegalArgumentException{
+        if(highPriorityFailure<0){
+            throw new IllegalArgumentException(String.format("The threshold highPriorityFailure should be positive or null, but was %d", highPriorityFailure));
+        }
+        if(highPriorityUnstable<0){
+            throw new IllegalArgumentException(String.format("The threshold highPriorityUnstable should be positive or null, but was %d", highPriorityUnstable));
+        }
+        if(normalPriorityFailure<0){
+            throw new IllegalArgumentException(String.format("The threshold normalPriorityFailure should be positive or null, but was %d", normalPriorityFailure));
+        }
+        if(normalPriorityUnstable<0){
+            throw new IllegalArgumentException(String.format("The threshold normalPriorityUnstable should be positive or null, but was %d", normalPriorityUnstable));
+        }
+        if(lowPriorityFailure<0){
+            throw new IllegalArgumentException(String.format("The threshold lowPriorityFailure should be positive or null, but was %d", lowPriorityFailure));
+        }
+        if(lowPriorityUnstable<0){
+            throw new IllegalArgumentException(String.format("The threshold lowPriorityUnstable should be positive or null, but was %d", lowPriorityUnstable));
+        }
+        if((lowPriorityFailure!=0 && lowPriorityUnstable!=0) && lowPriorityFailure <= lowPriorityUnstable){
+            throw new IllegalArgumentException(String.format("The threshold lowPriorityFailure has to be bigger than lowPriorityUnstable, but was %d and lowPriorityUnstable was %d.", lowPriorityFailure, lowPriorityUnstable));
+        }
+        if((normalPriorityFailure!=0 && normalPriorityUnstable!=0) && normalPriorityFailure <= normalPriorityUnstable){
+            throw new IllegalArgumentException(String.format("The threshold normalPriorityFailure has to be bigger than normalPriorityUnstable, but was %d and normalPriorityUnstable was %d.", normalPriorityFailure, normalPriorityUnstable));
+        }
+        if((highPriorityFailure!=0 && highPriorityUnstable!=0) && highPriorityFailure <= highPriorityUnstable){
+            throw new IllegalArgumentException(String.format("The threshold highPriorityFailure has to be bigger than highPriorityUnstable, but was %d and highPriorityUnstable was %d.", highPriorityFailure, highPriorityUnstable));
+        }
     }
 }
+
+
--- src/test/java/io/jenkins/plugins/analysis/core/quality/QualityGateEnforcerTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/quality/QualityGateEnforcerTest.java
@@ -1,11 +1,19 @@
 package io.jenkins.plugins.analysis.core.quality;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
+import com.google.common.collect.ImmutableMap;
+
+import static com.google.common.primitives.Ints.asList;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import hudson.model.Result;
+import hudson.plugins.analysis.util.model.Priority;
 
 /**
  * Tests the class {@link QualityGateEnforcer}.
@@ -17,40 +25,318 @@ class QualityGateEnforcerTest {
     void shouldBeSuccessfulWhenNoIssuesPresentAndNoQualityGateIsSet() {
         QualityGateEnforcer enforcer = new QualityGateEnforcer();
         StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        QualityGate qualityGate = new QualityGateBuilder().build();
+
+        Result success = enforcer.evaluate(run, qualityGate);
+
+        assertThat(success)
+                .as("No issues and no quality gate should always be a SUCCESS.")
+                .isEqualTo(Result.SUCCESS);
+    }
+
+    @Test
+    void shouldBeSuccessfulWhenNoIssuesPresentAndHighPriorityFailureQualityGateIsSet() {
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        QualityGate qualityGate = new QualityGateBuilder().setHighPriorityFailure(1).build();
+
+        Result success = enforcer.evaluate(run, qualityGate);
+
+        assertThat(success)
+                .as("No issues and high priority quality gate failure should always be a SUCCESS.")
+                .isEqualTo(Result.SUCCESS);
+    }
+
+    @Test
+    void shouldBeSuccessfulWhenNoIssuesPresentAndNormalPriorityFailureQualityGateIsSet() {
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        QualityGate qualityGate = new QualityGateBuilder().setNormalPriorityFailure(1).build();
+
+        Result success = enforcer.evaluate(run, qualityGate);
+
+        assertThat(success)
+                .as("No issues and normal priority quality gate failure should always be a SUCCESS.")
+                .isEqualTo(Result.SUCCESS);
+    }
+
+    @Test
+    void shouldBeSuccessfulWhenNoIssuesPresentAndLowPriorityFailureQualityGateIsSet() {
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        QualityGate qualityGate = new QualityGateBuilder().setLowPriorityFailure(1).build();
+
+        Result success = enforcer.evaluate(run, qualityGate);
+
+        assertThat(success)
+                .as("No issues and low priority quality gate failure should always be a SUCCESS.")
+                .isEqualTo(Result.SUCCESS);
+    }
+
+    @Test
+    void shouldBeSuccessfulWhenNoIssuesPresentAndHighPriorityUnstableQualityGateIsSet() {
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        QualityGate qualityGate = new QualityGateBuilder().setHighPriorityUnstable(1).build();
+
+        Result success = enforcer.evaluate(run, qualityGate);
+
+        assertThat(success)
+                .as("No issues and high priority quality gate unstable should always be a SUCCESS.")
+                .isEqualTo(Result.SUCCESS);
+    }
+
+    @Test
+    void shouldBeSuccessfulWhenNoIssuesPresentAndNormalPriorityUnstableQualityGateIsSet() {
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        QualityGate qualityGate = new QualityGateBuilder().setNormalPriorityUnstable(1).build();
+
+        Result success = enforcer.evaluate(run, qualityGate);
+
+        assertThat(success)
+                .as("No issues and normal priority quality gate unstable should always be a SUCCESS.")
+                .isEqualTo(Result.SUCCESS);
+    }
+
+    @Test
+    void shouldBeSuccessfulWhenNoIssuesPresentAndLowPriorityUnstableQualityGateIsSet() {
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        QualityGate qualityGate = new QualityGateBuilder().setLowPriorityUnstable(1).build();
+
+        Result success = enforcer.evaluate(run, qualityGate);
+
+        assertThat(success)
+                .as("No issues and low priority quality gate unstable should always be a SUCCESS.")
+                .isEqualTo(Result.SUCCESS);
+    }
+
+    @Test
+    void shouldBeSuccessfulWhenNoIssuesPresentAndAllPrioritysQualityGateIsSet(){
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        QualityGate qualityGate = new QualityGateBuilder().setHighPriorityFailure(2).setHighPriorityUnstable(1)
+                .setNormalPriorityFailure(2).setNormalPriorityUnstable(1)
+                .setLowPriorityFailure(2).setLowPriorityUnstable(1).build();
+        Result success = enforcer.evaluate(run, qualityGate);
+
+        assertThat(success)
+                .as("No issues and all priority gates are set should always be a SUCCESS.")
+                .isEqualTo(Result.SUCCESS);
+    }
+
+    @Test
+    void shouldFailBuildIfHighQualityFailureThresholdIsSet() {
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        when(run.getTotalHighPrioritySize()).thenReturn(1);
+
+        QualityGate qualityGate = new QualityGateBuilder().setHighPriorityFailure(1).build();
+
+        Result failure = enforcer.evaluate(run, qualityGate);
+
+        assertThat(failure)
+                .as("One high failure issue should return FAILURE if high failure quality gate is set.")
+                .isEqualTo(Result.FAILURE);
+    }
+
+    @Test
+    void shouldFailBuildIfNormalQualityFailureThresholdIsSet() {
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        when(run.getTotalNormalPrioritySize()).thenReturn(1);
+
+        QualityGate qualityGate = new QualityGateBuilder().setNormalPriorityFailure(1).build();
+
+        Result failure = enforcer.evaluate(run, qualityGate);
+
+        assertThat(failure)
+                .as("One normal failure issue should return FAILURE if normal failure quality gate is set.")
+                .isEqualTo(Result.FAILURE);
+    }
+
+    @Test
+    void shouldFailBuildIfLowQualityFailureThresholdIsSet() {
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        when(run.getTotalLowPrioritySize()).thenReturn(1);
+
+        QualityGate qualityGate = new QualityGateBuilder().setLowPriorityFailure(1).build();
+
+        Result failure = enforcer.evaluate(run, qualityGate);
+
+        assertThat(failure)
+                .as("One low failure issue should return FAILURE if low failure quality gate is set.")
+                .isEqualTo(Result.FAILURE);
+    }
+
+    @Test
+    void shouldBeUnstableBuildIfHighQualityUnstableThresholdIsSet() {
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        when(run.getTotalHighPrioritySize()).thenReturn(1);
+
+        QualityGate qualityGate = new QualityGateBuilder().setHighPriorityUnstable(1).build();
+
+        Result failure = enforcer.evaluate(run, qualityGate);
+
+        assertThat(failure)
+                .as("One high failure issue should return UNSTABLE if high unstable quality gate is set.")
+                .isEqualTo(Result.UNSTABLE);
+    }
+
+    @Test
+    void shouldBeUnstableBuildIfNormalQualityUnstableThresholdIsSet() {
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        when(run.getTotalNormalPrioritySize()).thenReturn(1);
+
+        QualityGate qualityGate = new QualityGateBuilder().setNormalPriorityUnstable(1).build();
+
+        Result failure = enforcer.evaluate(run, qualityGate);
+
+        assertThat(failure)
+                .as("One normal failure issue should return UNSTABLE if normal unstable quality gate is set.")
+                .isEqualTo(Result.UNSTABLE);
+    }
+
+    @Test
+    void shouldBeUnstableBuildIfLowQualityUnstableThresholdIsSet() {
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        when(run.getTotalLowPrioritySize()).thenReturn(1);
+
+        QualityGate qualityGate = new QualityGateBuilder().setLowPriorityUnstable(1).build();
+
+        Result failure = enforcer.evaluate(run, qualityGate);
+
+        assertThat(failure)
+                .as("One low failure issue should return UNSTABLE if low unstable quality gate is set.")
+                .isEqualTo(Result.UNSTABLE);
+    }
+
+    @Test
+    void shouldSuccessIfNoFailureTresholdIsSet(){
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        when(run.getTotalSize()).thenReturn(1);
+
         QualityGate qualityGate = new QualityGate();
 
         Result success = enforcer.evaluate(run, qualityGate);
 
         assertThat(success)
-                .as("No issues and no quality gate should always be a SUCCESS")
+                .as("One issue without threshold should return a SUCCESS")
                 .isEqualTo(Result.SUCCESS);
     }
 
     @Test
-    void shouldBeSuccessfulWhenNoIssuesPresentAndFailureQualityGateIsSet() {
-        QualityGateEnforcer enforcer = new QualityGateEnforcer();
-        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
-        QualityGate qualityGate = new QualityGate(1);
-
-        Result success = enforcer.evaluate(run, qualityGate);
-
-        assertThat(success)
-                .as("No issues and any failure quality gate should always be a SUCCESS")
-                .isEqualTo(Result.SUCCESS);
-    }
-
-    @Test
-    void shouldFailBuildIfFailureThresholdIsSet() {
+    void shouldSuccessIfWarningWithLowPriorityAndThresholdIsHighPriority(){
         QualityGateEnforcer enforcer = new QualityGateEnforcer();
         StaticAnalysisRun run = mock(StaticAnalysisRun.class);
         when(run.getTotalSize()).thenReturn(1);
+        when(run.getTotalHighPrioritySize()).thenReturn(0);
+        when(run.getTotalLowPrioritySize()).thenReturn(1);
 
-        QualityGate qualityGate = new QualityGate(1);
+        QualityGate qualityGate = new QualityGateBuilder().setHighPriorityFailure(1).build();
 
-        Result failure = enforcer.evaluate(run, qualityGate);
+        Result success = enforcer.evaluate(run, qualityGate);
+        assertThat(success)
+                .as("One issue with low priority but threshold is high priority should return a SUCCESS")
+                .isEqualTo(Result.SUCCESS);
+    }
 
-        assertThat(failure)
-                .as("One issue should return a FAILURE")
-                .isEqualTo(Result.FAILURE);
+    @Test
+    void shouldReturnExceptionWhenHighPriorityFailureIsNegative(){
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        when(run.getTotalSize()).thenReturn(1);
+        when(run.getTotalHighPrioritySize()).thenReturn(1);
+
+        assertThatThrownBy(()->{new QualityGateBuilder().setHighPriorityFailure(-1).build();})
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void shouldReturnExceptionWhenHighPriorityUnstableIsNegative(){
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        when(run.getTotalSize()).thenReturn(1);
+        when(run.getTotalHighPrioritySize()).thenReturn(1);
+
+        assertThatThrownBy(()->{new QualityGateBuilder().setHighPriorityUnstable(-1).build();})
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void shouldReturnExceptionWhenNormalPriorityFailureIsNegative(){
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        when(run.getTotalSize()).thenReturn(1);
+        when(run.getTotalNormalPrioritySize()).thenReturn(1);
+
+        assertThatThrownBy(()->{new QualityGateBuilder().setNormalPriorityFailure(-1).build();})
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void shouldReturnExceptionWhenNormalPriorityUnstableIsNegative(){
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        when(run.getTotalSize()).thenReturn(1);
+        when(run.getTotalNormalPrioritySize()).thenReturn(1);
+
+        assertThatThrownBy(()->{new QualityGateBuilder().setNormalPriorityUnstable(-1).build();})
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void shouldReturnExceptionWhenLowPriorityFailureIsNegative(){
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        when(run.getTotalSize()).thenReturn(1);
+        when(run.getTotalLowPrioritySize()).thenReturn(1);
+
+        assertThatThrownBy(()->{new QualityGateBuilder().setLowPriorityFailure(-1).build();})
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void shouldReturnExceptionWhenLowPriorityUnstableIsNegative(){
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+        when(run.getTotalSize()).thenReturn(1);
+        when(run.getTotalLowPrioritySize()).thenReturn(1);
+
+        assertThatThrownBy(()->{new QualityGateBuilder().setLowPriorityUnstable(-1).build();})
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void schouldReturnExceptionWhenHighPriorityFailureSmallerThanHighPriorityUnstable(){
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+
+        assertThatThrownBy(()->{new QualityGateBuilder().setHighPriorityFailure(1).setHighPriorityUnstable(2).build();})
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void schouldReturnExceptionWhenNormalPriorityFailureSmallerThanNormalPriorityUnstable(){
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+
+        assertThatThrownBy(()->{new QualityGateBuilder().setNormalPriorityFailure(1).setNormalPriorityUnstable(2).build();})
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void schouldReturnExceptionWhenLowPriorityFailureSmallerThanLowPriorityUnstable(){
+        QualityGateEnforcer enforcer = new QualityGateEnforcer();
+        StaticAnalysisRun run = mock(StaticAnalysisRun.class);
+
+        assertThatThrownBy(()->{new QualityGateBuilder().setLowPriorityFailure(1).setLowPriorityUnstable(2).build();})
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
