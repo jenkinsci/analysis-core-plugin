dfbd28ac4c0bc8d7f8b0ea9341f2c2bb9a251569
diridari <Kletern1>
2017-12-04 09:36:39 +0100

    do required changes

--- src/test/java/io/jenkins/plugins/analysis/core/graphs/HealthSeriesBuilderTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/graphs/HealthSeriesBuilderTest.java
@@ -6,30 +6,48 @@ import org.junit.jupiter.api.Test;
 
 import io.jenkins.plugins.analysis.core.quality.HealthDescriptor;
 import io.jenkins.plugins.analysis.core.quality.StaticAnalysisRun;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 /**
  * Test Class for HealthSeriesBuilder.
  */
-class HealthSeriesBuilderTest {
+class HealthSeriesBuilderTest extends SerialBuilderTest{
 
 
     /**
-     * generate a stub parameter.
+     * generate a StaticAnalysisRun stub.
+     * the stub returns for the method getTotalSize 0,1,10,20
      *
-     * @return stub parameter
+     * @return stub StaticAnalysisRun
      */
-    private StaticAnalysisRun generateParamStub() {
+    private StaticAnalysisRun generateStaticAnalysisRunStub() {
+        return generateStaticAnalysisRunStub(0, 1, 10, 20);
+    }
+
+    /**
+     * generate a StaticAnalysisRun stub.
+     * the stub returns for the method getTotalSize the given values
+     *
+     * @param firstSize  first return value of getTotalSize
+     * @param additionalSize list of Integers with additional getTotalSize
+     * @return stub StaticAnalysisRun
+     */
+    private StaticAnalysisRun generateStaticAnalysisRunStub(int firstSize, Integer... additionalSize) {
         StaticAnalysisRun paramStub = mock(StaticAnalysisRun.class);
-        when(paramStub.getTotalSize()).thenReturn(0).thenReturn(1).thenReturn(10).thenReturn(20);
+        when(paramStub.getTotalSize()).thenReturn(firstSize, additionalSize);
         return paramStub;
     }
 
-    @Test
-    void healthReportWithDisabledHealthDescriptor() {
-        HealthDescriptor healthDiscStub = GenerateHealthStub.generateHealthDescriptor(false, 0, 0);
-        StaticAnalysisRun paramStub = generateParamStub();
+    /**
+     * computeSeries with not enabled Health Descriptor.
+     * @param healthy number of healthy
+     * @param unhealthy number of unhealthy
+     */
+    private void shouldFindSingleResultsFor(int healthy, int unhealthy){
+        HealthDescriptor healthDiscStub = generateHealthDescriptor(false,healthy,unhealthy);
+        StaticAnalysisRun paramStub = generateStaticAnalysisRunStub();
         HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
 
         assertThat(b.computeSeries(paramStub)).containsExactly(0);
@@ -37,29 +55,21 @@ class HealthSeriesBuilderTest {
         assertThat(b.computeSeries(paramStub)).containsExactly(10);
         assertThat(b.computeSeries(paramStub)).containsExactly(20);
     }
+    @Test
+    void healthReportWithDisabledHealthDescriptor() {
+        shouldFindSingleResultsFor(0,0);
+    }
 
     @Test
     void healthReportWithDisabledHealthDescriptorAndHealthyDefinition() {
-        HealthDescriptor healthDiscStub = GenerateHealthStub.generateHealthDescriptor(false, 9, 10);
-        StaticAnalysisRun paramStub = generateParamStub();
-        HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
-
-        List<Integer> firstList = b.computeSeries(paramStub);
-        List<Integer> secondList = b.computeSeries(paramStub);
-        List<Integer> thirdList = b.computeSeries(paramStub);
-        List<Integer> fourthList = b.computeSeries(paramStub);
-
-        assertThat(firstList).containsExactly(0);
-        assertThat(secondList).containsExactly(1);
-        assertThat(thirdList).containsExactly(10);
-        assertThat(fourthList).containsExactly(20);
+        shouldFindSingleResultsFor(9,10);
 
     }
 
     @Test
     void healthReportWithNoPreviousHeath() {
-        HealthDescriptor healthDiscStub = GenerateHealthStub.generateHealthDescriptor(true, 0, 0);
-        StaticAnalysisRun paramStub = generateParamStub();
+        HealthDescriptor healthDiscStub = generateHealthDescriptor(true, 0, 0);
+        StaticAnalysisRun paramStub = generateStaticAnalysisRunStub();
         HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
 
         List<Integer> firstList = b.computeSeries(paramStub);
@@ -75,8 +85,8 @@ class HealthSeriesBuilderTest {
 
     @Test
     void healthReportHealthyEqUnhealthy() {
-        HealthDescriptor healthDiscStub = GenerateHealthStub.generateHealthDescriptor(true, 9, 9);
-        StaticAnalysisRun paramStub = generateParamStub();
+        HealthDescriptor healthDiscStub = generateHealthDescriptor(true, 8, 8);
+        StaticAnalysisRun paramStub = generateStaticAnalysisRunStub();
         HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
 
         List<Integer> firstList = b.computeSeries(paramStub);
@@ -86,15 +96,33 @@ class HealthSeriesBuilderTest {
 
         assertThat(firstList).containsExactly(0, 0, 0);
         assertThat(secondList).containsExactly(1, 0, 0);
-        assertThat(thirdList).containsExactly(9, 0, 1);
-        assertThat(fourthList).containsExactly(9, 0, 11);
+        assertThat(thirdList).containsExactly(8, 0, 2);
+        assertThat(fourthList).containsExactly(8, 0, 12);
 
     }
 
     @Test
-    void healthReportHealthyOneLessUnhealthy() {
-        HealthDescriptor healthDiscStub = GenerateHealthStub.generateHealthDescriptor(true, 9, 10);
-        StaticAnalysisRun paramStub = generateParamStub();
+    void healthReportWithUnhealthyBelowBorder() {
+        HealthDescriptor healthDiscStub = generateHealthDescriptor(true, 8, 9);
+        StaticAnalysisRun paramStub = generateStaticAnalysisRunStub();
+        HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
+
+        List<Integer> firstList = b.computeSeries(paramStub);
+        List<Integer> secondList = b.computeSeries(paramStub);
+        List<Integer> thirdList = b.computeSeries(paramStub);
+        List<Integer> fourthList = b.computeSeries(paramStub);
+
+        assertThat(firstList).containsExactly(0, 0, 0);
+        assertThat(secondList).containsExactly(1, 0, 0);
+        assertThat(thirdList).containsExactly(8, 1, 1);
+        assertThat(fourthList).containsExactly(8, 1, 11);
+
+    }
+
+    @Test
+    void healthReportUnHealthyOnBorder() {
+        HealthDescriptor healthDiscStub = generateHealthDescriptor(true, 9, 10);
+        StaticAnalysisRun paramStub = generateStaticAnalysisRunStub();
         HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
 
         List<Integer> firstList = b.computeSeries(paramStub);
@@ -109,10 +137,11 @@ class HealthSeriesBuilderTest {
 
     }
 
+
     @Test
-    void healthReportMoreUnhealthyThanHealthy() {
-        HealthDescriptor healthDiscStub = GenerateHealthStub.generateHealthDescriptor(true, 9, 15);
-        StaticAnalysisRun paramStub = generateParamStub();
+    void healthReportUnhealthyOverBorder() {
+        HealthDescriptor healthDiscStub = generateHealthDescriptor(true, 9, 11);
+        StaticAnalysisRun paramStub = generateStaticAnalysisRunStub();
         HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
 
         List<Integer> firstList = b.computeSeries(paramStub);
@@ -123,20 +152,53 @@ class HealthSeriesBuilderTest {
         assertThat(firstList).containsExactly(0, 0, 0);
         assertThat(secondList).containsExactly(1, 0, 0);
         assertThat(thirdList).containsExactly(9, 1, 0);
-        assertThat(fourthList).containsExactly(9, 6, 5);
+        assertThat(fourthList).containsExactly(9, 2, 9);
 
     }
+    @Test
+    void healthReportHealthyOnBorder() {
+        HealthDescriptor healthDiscStub = generateHealthDescriptor(true, 10, 13);
+        StaticAnalysisRun paramStub = generateStaticAnalysisRunStub();
+        HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
 
+        List<Integer> firstList = b.computeSeries(paramStub);
+        List<Integer> secondList = b.computeSeries(paramStub);
+        List<Integer> thirdList = b.computeSeries(paramStub);
+        List<Integer> fourthList = b.computeSeries(paramStub);
+
+        assertThat(firstList).containsExactly(0, 0, 0);
+        assertThat(secondList).containsExactly(1, 0, 0);
+        assertThat(thirdList).containsExactly(10, 0, 0);
+        assertThat(fourthList).containsExactly(10, 3, 7);
+
+    }
+    @Test
+    void healthReportHealthyOverBorder() {
+        HealthDescriptor healthDiscStub = generateHealthDescriptor(true, 11, 13);
+        StaticAnalysisRun paramStub = generateStaticAnalysisRunStub();
+        HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
+
+        List<Integer> firstList = b.computeSeries(paramStub);
+        List<Integer> secondList = b.computeSeries(paramStub);
+        List<Integer> thirdList = b.computeSeries(paramStub);
+        List<Integer> fourthList = b.computeSeries(paramStub);
+
+        assertThat(firstList).containsExactly(0, 0, 0);
+        assertThat(secondList).containsExactly(1, 0, 0);
+        assertThat(thirdList).containsExactly(10, 0, 0);
+        assertThat(fourthList).containsExactly(11, 2, 7);
+
+    }
     @Test
     void GetFirstReminderBoarder() {
-        HealthDescriptor healthDiscStub = GenerateHealthStub.generateHealthDescriptor(true, 1, 0);
+        HealthDescriptor healthDiscStub = generateHealthDescriptor(true, 1, 0);
         StaticAnalysisRun paramStub = mock(StaticAnalysisRun.class);
         when(paramStub.getTotalSize()).thenReturn(1);
         HealthSeriesBuilder b = new HealthSeriesBuilder(healthDiscStub);
         List<Integer> first = b.computeSeries(paramStub);
 
         assertThat(first).containsExactly(1, 0, 0);
-        assertThat(first.size()).isEqualTo(3);
+        assertThat(first).hasSize(3);
     }
 
 
--- src/test/java/io/jenkins/plugins/analysis/core/graphs/SerialBuilderTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/graphs/SerialBuilderTest.java
@@ -8,6 +8,8 @@ import java.util.GregorianCalendar;
 import java.util.List;
 
 
+import io.jenkins.plugins.analysis.core.quality.HealthDescriptor;
+import net.bytebuddy.implementation.bytecode.Throw;
 import org.apache.commons.lang3.ArrayUtils;
 import org.jfree.data.category.CategoryDataset;
 import org.junit.jupiter.api.Test;
@@ -22,31 +24,55 @@ import static org.mockito.Mockito.*;
  * Test Class for SerialBuilder.
  */
 class SerialBuilderTest {
-    final String assertErrorMessage = "\nCurrent Build is the <%d> failed to check:";
-
+    /**
+     * Generate a stub HealthDescriptor.
+     *
+     * @param enabled
+     *         stub return this on isEnabled
+     * @param healthy
+     *         stub return this on getHealthy
+     * @param unhealthy
+     *         stub return this on getUnHealthy
+     *
+     * @return stub HealthDescriptor
+     */
+    static HealthDescriptor generateHealthDescriptor(boolean enabled, int healthy, int unhealthy) {
+        HealthDescriptor healthDiscStub = mock(HealthDescriptor.class);
+        when(healthDiscStub.isEnabled()).thenReturn(enabled);
+        when(healthDiscStub.getHealthy()).thenReturn(healthy);
+        when(healthDiscStub.getUnHealthy()).thenReturn(unhealthy);
+        return healthDiscStub;
+    }
+    /**
+     * Static count of the number of builds
+     */
     private static int buildCounter = 0;
 
     /**
      * generate list of mocks.
      *
-     * @param buildTime
-     *         (day passed ,Size)*x
+     * @param passedDays  list of the passwd days one per build
+     * @param sizeOfBuild list of the size of each build
+     * @return list of StaticAnalysisRun
      */
-    private ArrayList<StaticAnalysisRun> getIterator(long... buildTime) {
+    private ArrayList<StaticAnalysisRun> getIterator(List<Integer> passedDays, List<Integer> sizeOfBuild) {
+        if (passedDays.size() != sizeOfBuild.size()) {
+            throw new IllegalArgumentException("Exepect, that SerialBuilderTest.getIterator() gets two list with the same size");
+        }
         final int dayMS = 24 * 3600 * 1000;
         ArrayList<StaticAnalysisRun> list = new ArrayList<>();
-        for (int i = 0; i < buildTime.length; i += 2) {
+        for (int i = 0; i < passedDays.size(); i++) {
             StaticAnalysisRun stub = mock(StaticAnalysisRun.class);
 
             Calendar today = new GregorianCalendar();
             AnalysisBuild build = mock(AnalysisBuild.class);
-            when(build.getNumber()).thenReturn(buildTime.length - i / 2);
+            when(build.getNumber()).thenReturn(passedDays.size() - i);
 
             when(build.getDisplayName()).thenReturn("magic" + (buildCounter += 2)); // some build are removed
-            when(build.getTimeInMillis()).thenReturn(today.getTimeInMillis() - buildTime[i] * dayMS);
+            when(build.getTimeInMillis()).thenReturn(today.getTimeInMillis() - passedDays.get(i) * dayMS);
             when(stub.getBuild()).thenReturn(build);
 
-            when(stub.getTotalSize()).thenReturn((int) buildTime[i + 1]);
+            when(stub.getTotalSize()).thenReturn((int) sizeOfBuild.get(i));
             list.add(stub);
         }
 
@@ -54,14 +80,36 @@ class SerialBuilderTest {
         return list;
     }
 
+    /**
+     * Build a ArrayList with the given values.
+     * @param parameter list of ints to add
+     * @return arrayList with the values
+     */
+    private ArrayList<Integer> buildSet(int... parameter) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int value : parameter) {
+            list.add(value);
+        }
+        return list;
+    }
+
+    /**
+     * Check if all values in dataset are correct.
+     * @param data dataSet
+     * @param healthy healthy value
+     * @param unHealthy unhelaty value
+     * @param list list of expected values
+     */
     private void checkDataSet(CategoryDataset data, int healthy, int unHealthy, int... list) {
+        String assertErrorMessage = "\nCurrent Build is the <%d> failed to check:";
         ArrayUtils.reverse(list);
         assertThat(data.getColumnCount()).isEqualTo(list.length);
         for (int i = 0; i < list.length; i++) {
             StaticAnalysisRun stub = mock(StaticAnalysisRun.class);
             when(stub.getTotalSize()).thenReturn(list[i]);
-            List<Integer> result = new HealthSeriesBuilder(GenerateHealthStub.
-                    generateHealthDescriptor(true, healthy, unHealthy)).computeSeries(stub);
+            List<Integer> result = new HealthSeriesBuilder(generateHealthDescriptor(true, healthy, unHealthy))
+                    .computeSeries(stub);
+
             assertThat(data.getValue(0, i)).as(assertErrorMessage + " <healthy>\n", i)
                     .isEqualTo(result.get(0));
             assertThat(data.getValue(1, i)).as(assertErrorMessage + " <medium healthy>\n", i)
@@ -71,6 +119,13 @@ class SerialBuilderTest {
         }
     }
 
+    /**
+     * generate a GraphConfiguration stub.
+     * @param asDomain should build as Domain
+     * @param dayCount number of dayCount if 0 the daycount is disabled
+     * @param buildCount number of build of 0 build count is disabled
+     * @return GraphConfiguration stub
+     */
     private GraphConfiguration generateGraphConfiguration(boolean asDomain, int dayCount, int buildCount) {
         GraphConfiguration stub = mock(GraphConfiguration.class);
         when(stub.useBuildDateAsDomain()).thenReturn(asDomain);
@@ -85,10 +140,11 @@ class SerialBuilderTest {
     void dataSetWithActiveDayCountAndBuildNotAsDomain() {
         GraphConfiguration stub = generateGraphConfiguration(false, 10, 0);
 
-        HealthSeriesBuilder b = new HealthSeriesBuilder(GenerateHealthStub.generateHealthDescriptor(true, 5, 8));
-        long[] values = {1, 0, 2, 8, 3, 10, 4, 16, 20, 99};
-        int[] newValues = {0, 8, 10, 16};
-        CategoryDataset t = b.createDataSet(stub, getIterator(values));
+        HealthSeriesBuilder b = new HealthSeriesBuilder(generateHealthDescriptor(true, 5, 8));
+        ArrayList<Integer> days = buildSet(1,2,3,4,10,11);
+        ArrayList<Integer> buildSize = buildSet(0,8,10,16,20,99);
+        int[] newValues = {0, 8, 10, 16,20};
+        CategoryDataset t = b.createDataSet(stub, getIterator(days,buildSize));
         checkDataSet(t, 5, 8, newValues);
 
     }
@@ -97,10 +153,11 @@ class SerialBuilderTest {
     void dataSetWithBuildNotAsDomainAndWithBuildCount() {
         GraphConfiguration stub = generateGraphConfiguration(false, 0, 3);
 
-        HealthSeriesBuilder b = new HealthSeriesBuilder(GenerateHealthStub.generateHealthDescriptor(true, 10, 20));
-        long[] values = {1, 0, 2, 8, 3, 10, 4, 16, 20, 99};
+        HealthSeriesBuilder b = new HealthSeriesBuilder(generateHealthDescriptor(true, 10, 20));
+        ArrayList<Integer> days = buildSet(1,2,3,4,11);
+        ArrayList<Integer> buildSize = buildSet(0,8,10,16,99);
         int[] newValues = {0, 8, 10};
-        CategoryDataset t = b.createDataSet(stub, getIterator(values));
+        CategoryDataset t = b.createDataSet(stub, getIterator(days,buildSize));
         checkDataSet(t, 10, 20, newValues);
 
     }
@@ -109,10 +166,11 @@ class SerialBuilderTest {
     void dataSetWithBuildNotAsDomainAndWithBuildCountAndDayCount() {
         GraphConfiguration stub = generateGraphConfiguration(false, 10, 3);
 
-        HealthSeriesBuilder b = new HealthSeriesBuilder(GenerateHealthStub.generateHealthDescriptor(true, 5, 8));
-        long[] values = {1, 0, 2, 8, 3, 10, 4, 16, 20, 99};
+        HealthSeriesBuilder b = new HealthSeriesBuilder(generateHealthDescriptor(true, 5, 8));
+        ArrayList<Integer> days = buildSet(1,2,3,4,20);
+        ArrayList<Integer> buildSize = buildSet(0,8,10,16,99);
         int[] newValues = {0, 8, 10};
-        CategoryDataset t = b.createDataSet(stub, getIterator(values));
+        CategoryDataset t = b.createDataSet(stub, getIterator(days,buildSize));
         checkDataSet(t, 5, 8, newValues);
 
     }
@@ -120,10 +178,11 @@ class SerialBuilderTest {
     @Test
     void dataSetWithNoBuilds() {
         GraphConfiguration stub = generateGraphConfiguration(false, 0, 0);
-        HealthSeriesBuilder b = new HealthSeriesBuilder(GenerateHealthStub.generateHealthDescriptor(true, 5, 8));
-        long[] values = {};
+        HealthSeriesBuilder b = new HealthSeriesBuilder(generateHealthDescriptor(true, 5, 8));
+        ArrayList<Integer> days = buildSet();
+        ArrayList<Integer> buildSize = buildSet();
         int[] newValues = {};
-        CategoryDataset t = b.createDataSet(stub, getIterator(values));
+        CategoryDataset t = b.createDataSet(stub, getIterator(days,buildSize));
         checkDataSet(t, 5, 8, newValues);
 
     }
@@ -132,50 +191,56 @@ class SerialBuilderTest {
     @Test
     void dataSetAsDomainAndNoBuildLimitations() {
         GraphConfiguration stub = generateGraphConfiguration(true, 0, 0);
-        HealthSeriesBuilder b = new HealthSeriesBuilder(GenerateHealthStub.generateHealthDescriptor(true, 5, 8));
-        long[] values = {1, 10, 1, 16, 4, 20, 4, 22, 4, 24, 7, 50};
+        HealthSeriesBuilder b = new HealthSeriesBuilder(generateHealthDescriptor(true, 5, 8));
+        ArrayList<Integer> days = buildSet(1,1,4,4,4,7);
+        ArrayList<Integer> buildSize = buildSet(10,16,20,22,24,50);
         int[] newValues = {13, 22, 50};
-        CategoryDataset t = b.createDataSet(stub, getIterator(values));
+        CategoryDataset t = b.createDataSet(stub, getIterator(days,buildSize));
         checkDataSet(t, 5, 8, newValues);
     }
 
     @Test
     void dataSetAsDomainAndBuildLimitation() {
         GraphConfiguration stub = generateGraphConfiguration(true, 0, 4);
-        HealthSeriesBuilder b = new HealthSeriesBuilder(GenerateHealthStub.generateHealthDescriptor(true, 5, 8));
+        HealthSeriesBuilder b = new HealthSeriesBuilder(generateHealthDescriptor(true, 5, 8));
         long[] values = {1, 10, 1, 16, 4, 20, 4, 22, 4, 24, 7, 50};
+        ArrayList<Integer> days = buildSet(1,1,4,4,4,7);
+        ArrayList<Integer> buildSize = buildSet(10,16,20,22,24,50);
         int[] newValues = {13, 21};
-        CategoryDataset t = b.createDataSet(stub, getIterator(values));
+        CategoryDataset t = b.createDataSet(stub, getIterator(days,buildSize));
         checkDataSet(t, 5, 8, newValues);
     }
 
     @Test
     void dataSetAsDomainAndTimeLimitation() {
         GraphConfiguration stub = generateGraphConfiguration(true, 3, 0);
-        HealthSeriesBuilder b = new HealthSeriesBuilder(GenerateHealthStub.generateHealthDescriptor(true, 5, 8));
-        long[] values = {1, 10, 1, 16, 4, 20, 4, 22, 4, 24, 7, 50};
+        HealthSeriesBuilder b = new HealthSeriesBuilder(generateHealthDescriptor(true, 5, 8));
+        ArrayList<Integer> days = buildSet(1,1,4,4,4,7);
+        ArrayList<Integer> buildSize = buildSet(10,16,20,22,24,50);
         int[] newValues = {13};
-        CategoryDataset t = b.createDataSet(stub, getIterator(values));
+        CategoryDataset t = b.createDataSet(stub, getIterator(days,buildSize));
         checkDataSet(t, 5, 8, newValues);
     }
 
     @Test
     void dataSetAsDomainNoBuilds() {
         GraphConfiguration stub = generateGraphConfiguration(true, 4, 5);
-        HealthSeriesBuilder b = new HealthSeriesBuilder(GenerateHealthStub.generateHealthDescriptor(true, 5, 8));
-        long[] values = {};
+        HealthSeriesBuilder b = new HealthSeriesBuilder(generateHealthDescriptor(true, 5, 8));
+        ArrayList<Integer> days = buildSet();
+        ArrayList<Integer> buildSize = buildSet();
         int[] newValues = {};
-        CategoryDataset t = b.createDataSet(stub, getIterator(values));
+        CategoryDataset t = b.createDataSet(stub, getIterator(days,buildSize));
         checkDataSet(t, 5, 8, newValues);
     }
 
     @Test
     public void dataSetAsDomainAndNotTimeLimitationButSetInverse() {
         GraphConfiguration stub = generateGraphConfiguration(true, 3, 0);
-        HealthSeriesBuilder b = new HealthSeriesBuilder(GenerateHealthStub.generateHealthDescriptor(true, 5, 8));
-        long[] values = {7, 50, 4, 24, 4, 22, 4, 20, 1, 16, 1, 10};
+        HealthSeriesBuilder b = new HealthSeriesBuilder(generateHealthDescriptor(true, 5, 8));
+        ArrayList<Integer> days = buildSet(7,4,4,4,1,1);
+        ArrayList<Integer> buildSize = buildSet(50,24,22,20,16,10);
         int[] newValues = {};
-        CategoryDataset t = b.createDataSet(stub, getIterator(values));
+        CategoryDataset t = b.createDataSet(stub, getIterator(days,buildSize));
         checkDataSet(t, 5, 8, newValues);
     }
 
@@ -183,10 +248,11 @@ class SerialBuilderTest {
     @Test
     public void dataSetAsDomainAndTimeLimitationWithTimeBoundary() {
         GraphConfiguration stub = generateGraphConfiguration(true, 3, 0);
-        HealthSeriesBuilder b = new HealthSeriesBuilder(GenerateHealthStub.generateHealthDescriptor(true, 5, 8));
-        long[] values = {1, 10, 1, 16, 3, 20, 3, 22, 3, 24, 7, 50};
+        HealthSeriesBuilder b = new HealthSeriesBuilder(generateHealthDescriptor(true, 5, 8));
+        ArrayList<Integer> days = buildSet(1,1,3,3,3,7);
+        ArrayList<Integer> buildSize = buildSet(10,16,20,22,24,50);
         int[] newValues = {13, 22};
-        CategoryDataset t = b.createDataSet(stub, getIterator(values));
+        CategoryDataset t = b.createDataSet(stub, getIterator(days,buildSize));
         checkDataSet(t, 5, 8, newValues);
     }
 
