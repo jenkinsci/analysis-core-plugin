360cc43bdc923eddb332461884de4bacb679c923
CorneliaChristof <cchristo@hm.edu>
2018-01-12 15:22:54 +0100

    removed target folder

--- target/analysis-core/js/tabs.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-        localStorage.setItem('activeTab', $(e.target).attr('href'));
-    });
-    var activeTab = localStorage.getItem('activeTab');
-    if (activeTab) {
-        $('#tab-details').find('a[href="' + activeTab + '"]').tab('show');
-    }
-})(jQuery);
