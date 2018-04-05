ca95272eac45bc4536fa05f519bdcc6aec7afd49
Anna-Maria Hardi <gallenbe@hm.edu>
2018-04-05 12:52:03 +0200

    Added a new test for the class EnvironmentResolver.

--- src/test/java/io/jenkins/plugins/analysis/core/util/EnvironmentResolverTest.java
+++ src/test/java/io/jenkins/plugins/analysis/core/util/EnvironmentResolverTest.java
@@ -1,9 +1,8 @@
 package io.jenkins.plugins.analysis.core.util;
 
 import hudson.EnvVars;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,24 +11,23 @@ import static edu.hm.hafner.analysis.assertj.Assertions.*;
 /**
  * Tests the class {@link EnvironmentResolver}.
  *
- * @author Elvira Hauer <hauer@hm.edu>
+ * @author Elvira Hauer
  */
 class EnvironmentResolverTest {
 	
 	private EnvironmentResolver sut;
 	private String aString;
-	
-	@Before
-    public void initialize() {
-       sut = new EnvironmentResolver();
-	   aString = "ein einfacher String";
+
+	@BeforeEach
+    void initialize(){
+        sut = new EnvironmentResolver();
+        aString  = "a simple String";
     }
-	
+
     @Test
     void testEnvVarsNull() {
         String result = sut.expandEnvironmentVariables(null, aString);
-       
-	    assertThat(result).isNotEmpty();
+        assertThat(result).isNotEmpty();
         assertThat(result).isEqualTo(aString);
         assertThat(result).hasSize(aString.length());
     }
@@ -37,7 +35,6 @@ class EnvironmentResolverTest {
     @Test
     void testEnvVarsEmpty() {
         String result = sut.expandEnvironmentVariables(new EnvVars(), aString);
-
         assertThat(result).isNotEmpty();
         assertThat(result).isEqualTo(aString);
         assertThat(result).hasSize(aString.length());
@@ -46,22 +43,20 @@ class EnvironmentResolverTest {
     @Test
     void testNonExpandedIsBlank() {
         String result = sut.expandEnvironmentVariables(new EnvVars(), "");
-
         assertThat(result).isEmpty();
         assertThat(result).isEqualTo("");
     }
 
     @Test
-    void testValidTest() {
-	    Map aMap = new HashMap<String, String>();
-	    aMap.put("das", "ist");
-	    aMap.put("eine", "test");
+    void testReplaceInNonExpanded() {
+	    Map<String, String> aMap = new HashMap<>();
+	    aMap.put("this", "is");
+	    aMap.put("a", "test");
 	    aMap.put("EnvVars", "Map");
         EnvVars envVars = new EnvVars(aMap);
-        String result = sut.expandEnvironmentVariables(envVars, "");
-        //Keine ahnung was passiert
-        assertThat(result).isEmpty();
-        assertThat(result).isEqualTo("");
+        String result = sut.expandEnvironmentVariables(envVars, "Hello replace the $this with is.");
+        assertThat(result).isNotEmpty();
+        assertThat(result).isEqualTo("Hello replace the is with is.");
     }
 
 }
